d2bcaabd36449b6c2fad9f03d1cb1adf540e2611
Zakaria Mansouri <zaqo.cs@outlook.com>
2020-04-15 23:38:49 +0400

    setting up React for frontend

---
+++ frontend/src/apps/main/entry/webpack.plugins.js
@@ -0,0 +1,23 @@
+// required modules
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// setting up project configurations and some env variables
+const t9config = require("../../../../t9config.js");
+const app = { ...t9config.apps.main, name: "main" };
+const appPath = app.basePath || app.name;
+const isDevelopment = process.env.NODE_ENV === "development";
+const fbpCode = app.analytics.facebook;
+const gaCode = app.analytics.google;
+
+module.exports = () => [
+  new HtmlWebpackPlugin({
+    filename: "index.html",
+    template: `pug-loader!./src/apps/${app.name}/entry/index.pug`,
+    templateParameters: {
+      isDev: isDevelopment,
+      fbpCode,
+      gaCode,
+    },
+  }),
+];
---
+++ frontend/t9config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  bundles: {
+    distFolder: "./firebase/public",
+    publicPath: "/",
+    publicResourcesPath: "w/",
+  },
+  apps: {
+    main: {
+      analytics: {
+        facebook: "xxxxxxxxxxxxxxx",
+        google: "UA-163554556-1",
+      },
+    },
+  },
+};
