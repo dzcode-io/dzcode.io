115fcfb6662844f5f667666c7549d9c574c589d4
Zakaria Mansouri <zaqo.cs@outlook.com>
2020-04-19 02:21:14 +0400

    Applied style for Landing scene components

--- frontend/src/apps/main/scenes/landing/join-classrooms/index.tsx
+++ frontend/src/apps/main/scenes/landing/join-classrooms/index.tsx
@@ -1,3 +1,6 @@
 import React from "react";
+import "./style";
 
-export const JoinClassrooms = () => <div>JoinClassrooms</div>;
+export const JoinClassrooms = () => (
+  <div className="join-classrooms">JoinClassrooms</div>
+);
