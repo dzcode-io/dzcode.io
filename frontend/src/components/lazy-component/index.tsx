f8fe4b7bd6b0202057a0db609ee335c61d617db0
Zakaria Mansouri <zaqo.cs@outlook.com>
2020-04-18 13:18:22 +0400

    refactored imports

--- frontend/src/components/lazy-component/index.tsx
+++ frontend/src/components/lazy-component/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { LoadingStatus } from "t9/types/fullstack";
 
 export const LazyComponent = (props: LazyComponentProps) => {
