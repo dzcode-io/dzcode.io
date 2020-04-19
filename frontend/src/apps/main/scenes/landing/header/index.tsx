0b04351d924d290843ca9a24989c99e4e921b649
Zakaria Mansouri <zaqo.cs@outlook.com>
2020-04-19 20:36:06 +0400

    updated landing components

--- frontend/src/apps/main/scenes/landing/header/index.tsx
+++ frontend/src/apps/main/scenes/landing/header/index.tsx
@@ -1,4 +1,12 @@
 import React from "react";
 import "./style";
 
-export const Header = () => <div className="header">Header</div>;
+export const Header = () => (
+  <div className="header">
+    <div className="shade" />
+    <div className="text">
+      <div className="title">dzCode.io</div>
+      <div className="description">Algerian Open Source Community</div>
+    </div>
+  </div>
+);
