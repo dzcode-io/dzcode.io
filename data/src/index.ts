9d0c6f48d17c7a46741b272ab939f52dd83c84fd
Zakaria Mansouri <zaqo.cs@outlook.com>
2020-04-22 13:33:07 +0400

    added collection route wirh returned data

--- data/src/index.ts
+++ data/src/index.ts
@@ -3,6 +3,11 @@ import { getDataEntry, getDataCollection } from "./utils/data";
 const app = express();
 const port = process.env.DATA_PORT || 9090;
 
+// Collections
+app.get("/:type/:collection(\\S+.json$)", (req, res) =>
+  res.json(getDataCollection(req.params.type, req.params.collection)),
+);
+
 // Entries
 app.get("/:type/:entry([\\/\\S]+[^\\/]$)", (req, res) =>
   res.json(getDataEntry(`${req.params.type}/${req.params.entry}`)),
