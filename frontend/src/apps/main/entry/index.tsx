46a69167c92ecdb387c9cce96110fc70a951ca49
Zakaria Mansouri <ZAQO.cs@outlook.com>
2020-04-22 23:04:49 +0400

    Revert "Testing Data Generator on staging"

--- frontend/src/apps/main/entry/index.tsx
+++ frontend/src/apps/main/entry/index.tsx
@@ -1,33 +1,12 @@
-import React from "react";
+import * as React from "react";
 import { render } from "react-dom";
 import "./style";
-import { Switch, Route, BrowserRouter, Redirect } from "react-router-dom";
-import { LazyComponent } from "src/components/lazy-component";
-import { Loading } from "src/components/loading";
-import { Navbar } from "t9/apps/main/components/navbar";
-import { Footer } from "t9/apps/main/components/footer";
 
 export const App: React.SFC<{}> = () => {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Switch>
-        <Route
-          path="/"
-          exact={true}
-          render={() => (
-            <LazyComponent
-              import={() =>
-                import(/* webpackChunkName: "landing" */ "../scenes/landing")
-              }
-              placeholder={Loading}
-            />
-          )}
-        />
-        <Route render={() => <Redirect to="/" />} />
-      </Switch>
-      <Footer />
-    </BrowserRouter>
+    <div>
+      Hello World, dzCode is under development, expect cools things soon
+    </div>
   );
 };
 
--- frontend/src/apps/main/scenes/landing/join-classrooms/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./style";
-
-export const JoinClassrooms = () => (
-  <div className="join-classrooms">JoinClassrooms</div>
-);
