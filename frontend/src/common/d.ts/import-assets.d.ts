308fcfacd554338c0a1bb7cd0610bb104acb8107
Zakaria Mansouri <zaqo.cs@outlook.com>
2020-04-23 12:55:08 +0400

    added declaration files for assets type of files

---
+++ frontend/src/common/d.ts/import-assets.d.ts
@@ -0,0 +1,44 @@
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpeg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.gif" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.svg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.ico" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.ttf" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.woff" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.woff2" {
+  const value: string;
+  export default value;
+}
