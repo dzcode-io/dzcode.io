b2c8e06988381aaada6df9cd85f9cb5d591412a5
Zakaria Mansouri <zaqo.cs@outlook.com>
2020-05-15 18:17:09 +0400

    Redux (Data Layer) for articles

--- frontend/src/apps/main/redux/reducers/index.ts
+++ frontend/src/apps/main/redux/reducers/index.ts
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
 import { documentation } from "./documentation";
 import { learnScene } from "./learn-scene";
+import { articles } from "./articles";
+import { articlesScene } from "./articles-scene";
 
 export const mainReducer = combineReducers({
   documentation,
   learnScene,
+  articles,
+  articlesScene,
 });
--- fullstack/src/types/index.ts
+++ fullstack/src/types/index.ts
@@ -11,4 +11,15 @@ export interface Document {
   views?: number;
 }
 
+export interface Article {
+  slug: string;
+  image?: string;
+  title: string;
+  description?: string;
+  content?: string;
+  authors?: string[];
+  contributors?: string[];
+  views?: number;
+}
+
 export type Environment = "development" | "staging" | "production";
