6460851951e60db10152909029848cc5153e4ef9
Zakaria Mansouri <zaqo.cs@outlook.com>
2020-05-15 18:29:45 +0400

    UI layer for articles

--- frontend/src/apps/main/scenes/articles/content/index.tsx
+++ frontend/src/apps/main/scenes/articles/content/index.tsx
@@ -1,4 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./style";
+import { Article } from "t9/types/fullstack";
 
-export const Content = () => <div className="content">Content</div>;
+export const Content = (props: ContentInterface) => {
+  useEffect(() => {
+    props.fetchCurrentArticle();
+  }, []);
+
+  return (
+    <div className="content">
+      {props.currentArticle ? (
+        <div>{props.currentArticle.title}</div>
+      ) : (
+        "Loading Article..."
+      )}
+    </div>
+  );
+};
+
+export interface ContentInterface {
+  fetchCurrentArticle: () => void;
+  currentArticle: Article | null;
+}
--- frontend/src/apps/main/scenes/articles/index.tsx
+++ frontend/src/apps/main/scenes/articles/index.tsx
@@ -1,21 +1,53 @@
+import { connect } from "react-redux";
 import React from "react";
 import { Sidebar } from "./sidebar";
 import { Content } from "./content";
-import { connect } from "react-redux";
 import { Article } from "t9/types/fullstack";
+import { useEffect } from "react";
+import { fetchArticlesList } from "t9/apps/main/redux/actions/articles-scene";
+import { fetchCurrentArticle } from "t9/apps/main/redux/actions/articles-scene";
+import { Route, useRouteMatch } from "react-router-dom";
 
-export const ArticlesScene = (props: any) => {
+export const ArticlesScene = (props: ArticlesScenePropsReduxed) => {
+  useEffect(() => {
+    props.fetchArticlesList();
+  }, []);
+
+  const { path } = useRouteMatch();
+
   return (
     <div className="articles">
-      <Sidebar />
-      <Content />
+      <Sidebar articlesList={props.articlesList} />
+      <Route
+        path={`${path}/:articleSlug`}
+        render={() => (
+          <Content
+            key={location.pathname}
+            currentArticle={props.currentArticle}
+            fetchCurrentArticle={props.fetchCurrentArticle}
+          />
+        )}
+      />
     </div>
   );
 };
+
+interface ArticlesScenePropsReduxed extends ArticlesSceneProps {
+  fetchArticlesList: () => void;
+  fetchCurrentArticle: () => void;
+}
 
 export interface ArticlesSceneProps {
   articlesList: Article[] | null;
   currentArticle: Article | null;
 }
 
-export default connect()(ArticlesScene);
+export default connect(
+  (state: { articlesScene: ArticlesSceneProps }) => ({
+    ...state.articlesScene,
+  }),
+  (dispatch: any) => ({
+    fetchArticlesList: () => dispatch(fetchArticlesList()),
+    fetchCurrentArticle: () => dispatch(fetchCurrentArticle()),
+  }),
+)(ArticlesScene);
