2c8a3a0f22fce715d3ab4293e8b8f2708f3d5d59
Zakaria Mansouri <zaqo.cs@outlook.com>
2020-05-15 17:25:41 +0400

    improved lazy loading using Suspense

--- frontend/src/apps/main/entry/index.tsx
+++ frontend/src/apps/main/entry/index.tsx
@@ -1,53 +1,27 @@
-import React from "react";
-import { render } from "react-dom";
 import "./style";
+import React, { lazy, Suspense } from "react";
+import { render } from "react-dom";
 import { Switch, Route, BrowserRouter, Redirect } from "react-router-dom";
-import { LazyComponent } from "src/components/lazy-component";
 import { Loading } from "src/components/loading";
 import { Navbar } from "t9/apps/main/components/navbar";
 import { Footer } from "t9/apps/main/components/footer";
+
+const Landing = lazy(() => import("t9/apps/main/scenes/landing"));
+const Blogs = lazy(() => import("t9/apps/main/scenes/blogs"));
+const Learn = lazy(() => import("t9/apps/main/scenes/learn"));
 
 export const App: React.SFC<{}> = () => {
   return (
     <BrowserRouter>
       <Navbar />
-      <Switch>
-        <Route
-          path="/"
-          exact={true}
-          render={() => (
-            <LazyComponent
-              import={() =>
-                import(/* webpackChunkName: "landing" */ "../scenes/landing")
-              }
-              placeholder={Loading}
-            />
-          )}
-        />
-        <Route
-          path="/Learn"
-          render={() => (
-            <LazyComponent
-              import={() =>
-                import(/* webpackChunkName: "learn" */ "../scenes/learn")
-              }
-              placeholder={Loading}
-            />
-          )}
-        />
-        <Route
-          path="/Blogs"
-          render={() => (
-            <LazyComponent
-              import={() =>
-                import(/* webpackChunkName: "blogs" */ "../scenes/blogs")
-              }
-              placeholder={Loading}
-            />
-          )}
-        />
-        <Route render={() => <Redirect to="/" />} />
-      </Switch>
+      <Suspense fallback={Loading}>
+        <Switch>
+          <Route path="/" exact={true} component={Landing} />
+          <Route path="/Learn" component={Learn} />
+          <Route path="/Blogs" component={Blogs} />
+          <Route render={() => <Redirect to="/" />} />
+        </Switch>
+      </Suspense>
       <Footer />
     </BrowserRouter>
   );
