e506a02e9b8f2f9611c4c9e2e2efca907ec101c2
Zakaria Mansouri <zaqo.cs@outlook.com>
2020-05-15 17:38:51 +0400

    UI Layer

--- frontend/src/apps/main/scenes/learn/content/index.tsx
+++ frontend/src/apps/main/scenes/learn/content/index.tsx
@@ -1,4 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./style";
+import { Document } from "t9/types/fullstack";
 
-export const Content = () => <div className="content">Content</div>;
+export const Content = (props: ContentInterface) => {
+  useEffect(() => {
+    props.fetchCurrentDocument();
+  }, []);
+
+  return (
+    <div className="content">
+      {props.currentDocument ? (
+        <div>{props.currentDocument.title}</div>
+      ) : (
+        "Loading Document..."
+      )}
+    </div>
+  );
+};
+
+export interface ContentInterface {
+  fetchCurrentDocument: () => void;
+  currentDocument: Document | null;
+}
