2c402e498a53d15c9b76aed67f3206c145568a52
Zakaria Mansouri <zaqo.cs@outlook.com>
2020-07-05 00:18:57 +0400

    💀 added the basic skeleton for Learn scene

--- frontend/src/apps/main/scenes/learn/content/index.tsx
+++ frontend/src/apps/main/scenes/learn/content/index.tsx
@@ -1,16 +1,36 @@
 import React, { useEffect } from "react";
-import "./style";
 import { Document } from "t9/types/fullstack";
+import Markdown from "react-markdown";
+import "./style";
 
 export const Content = (props: ContentInterface) => {
   useEffect(() => {
     props.fetchCurrentDocument();
   }, []);
-
+  const { currentDocument } = props;
   return (
     <div className="content">
-      {props.currentDocument ? (
-        <div>{props.currentDocument.title}</div>
+      {currentDocument ? (
+        <div>
+          {/* Image */}
+          {currentDocument.image && (
+            <img
+              className="hero-image"
+              src={currentDocument.image}
+              alt={currentDocument.title}
+            />
+          )}
+          {/* Title */}
+          <h2 className="title">{currentDocument.title}</h2>
+          {/* Description */}
+          <small className="description">{currentDocument.description}</small>
+          {/* Content */}
+          <Markdown className="content" source={currentDocument.content} />
+          {/* Contact + Edit*/}
+          <div className="actions">Contact + Edit document</div>
+          {/* Comments */}
+          <div className="comments">Comments</div>
+        </div>
       ) : (
         "Loading Document..."
       )}
