fb7b78cae95e5f2817e1ec5ca398fe0239cd82e7
Zakaria Mansouri <zaqo.cs@outlook.com>
2020-07-08 01:15:40 +0400

    added comments plugin

--- frontend/src/apps/main/scenes/learn/content/index.tsx
+++ frontend/src/apps/main/scenes/learn/content/index.tsx
@@ -3,9 +3,29 @@ import { Document } from "t9/types/fullstack";
 import Markdown from "react-markdown";
 import "./style";
 
+import programer from "../../landing/header/icons/programmer.png";
+import contact from "../../landing/header/icons/contact.png";
+import support from "../../landing/header/icons/support.png";
+import github from "../../landing/header/icons/github.png";
+
+const socialMedia = [
+  {
+    id: 1,
+    name: "dzcode",
+    href: "https://github.com/dzcode-io/dzcode.io",
+    icon: github,
+  },
+  { id: 2, name: "Learn", href: "/learn", icon: programer },
+  { id: 3, name: "Contact", href: "/contact", icon: contact },
+  { id: 4, name: "Support", href: "/support", icon: support },
+];
+
 export const Content = (props: ContentInterface) => {
   useEffect(() => {
     props.fetchCurrentDocument();
+    setTimeout(() => {
+      window.FB && window.FB.XFBML.parse();
+    }, 3000);
   }, []);
   const { currentDocument } = props;
   return (
@@ -27,10 +47,25 @@ export const Content = (props: ContentInterface) => {
           <hr className="break" />
           {/* Content */}
           <Markdown className="content" source={currentDocument.content} />
+          <hr className="break" />
           {/* Contact + Edit*/}
-          <div className="actions">Contact + Edit document</div>
+          <div className="actions">
+            {socialMedia.map((item) => {
+              return (
+                <div key={item.id} className="item">
+                  <img src={item.icon} alt={item.name} className="icon" />
+                  <a href={item.href}>{item.name}</a>
+                </div>
+              );
+            })}
+          </div>
           {/* Comments */}
-          <div className="comments">Comments</div>
+          <div
+            className="fb-comments"
+            data-href={location.origin + location.pathname}
+            data-width="100%"
+            data-numposts="5"
+          />
         </div>
       ) : (
         "Loading Document..."
