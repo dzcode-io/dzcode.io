98fce0d302281a175cb1daba8877c3fe782465c8
sohaib <sohaib.code@gmail.com>
2020-07-10 17:55:49 +0100

    add footer / fix overflow

--- frontend/src/apps/main/components/footer/index.tsx
+++ frontend/src/apps/main/components/footer/index.tsx
@@ -1,4 +1,105 @@
 import React from "react";
-import "./style";
+import "./style.scss";
 
-export const Footer = () => <div className="footer">Footer</div>;
+// Temp Data
+const Data = [
+  {
+    title: "Category",
+    links: [
+      {
+        href: "http://www.google.com",
+
+        text: "click me",
+      },
+      {
+        href: "http://www.google.com",
+
+        text: "click me",
+      },
+      {
+        href: "http://www.google.com",
+
+        text: "click me",
+      },
+      {
+        href: "http://www.google.com",
+
+        text: "click me",
+      },
+    ],
+  },
+  {
+    title: "Category",
+
+    links: [
+      {
+        href: "http://www.google.com",
+
+        text: "click me",
+      },
+      {
+        href: "http://www.google.com",
+
+        text: "click me",
+      },
+      {
+        href: "http://www.google.com",
+
+        text: "click me",
+      },
+      {
+        href: "http://www.google.com",
+
+        text: "click me",
+      },
+    ],
+  },
+  {
+    title: "Category",
+
+    links: [
+      {
+        href: "http://www.google.com",
+
+        text: "click me",
+      },
+      {
+        href: "http://www.google.com",
+
+        text: "click me",
+      },
+      {
+        href: "http://www.google.com",
+
+        text: "click me",
+      },
+      {
+        href: "http://www.google.com",
+        text: "click me",
+      },
+    ],
+  },
+];
+
+interface Props {}
+
+export const Footer: React.FC<Props> = ({}) => {
+  return (
+    <footer className="footer-grid ">
+      {Data.map((gridElemnt, i) => {
+        return (
+          <div className="footer-grid-item" key={i}>
+            <h2>{gridElemnt.title}</h2>
+            {gridElemnt.links.map((link, i) => {
+              return (
+                <a className="footer-grid-link" href={link.href} key={i}>
+                  {link.text}
+                </a>
+              );
+            })}
+          </div>
+        );
+      })}
+    </footer>
+  );
+};
