c9098908c7ca0c7ba1bb574313de0d6a1720de8f
sohaib <sohaib.code@gmail.com>
2020-07-14 21:47:06 +0100

    cors non tested

--- api/functions/src/index.ts
+++ api/functions/src/index.ts
@@ -2,7 +2,16 @@ import * as functions from "firebase-functions";
 import postEmail from "./email";
 import express, { Application } from "express";
 
+import cors = require("cors");
+
 const app: Application = express();
+
+const options = {
+  origin: "https://www.dzcode.io/",
+};
+
+//use cors middleware
+app.use(cors(options));
 
 app.post("/contact", postEmail);
 
