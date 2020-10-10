3add9f74bb0f7381158d4460935ad54c255829e7
sohaib <sohaib.code@gmail.com>
2020-10-10 14:16:15 +0100

    ✅ fix: refactor

--- frontend/src/apps/main/pages/projects/catalog/index.tsx
+++ frontend/src/apps/main/pages/projects/catalog/index.tsx
@@ -1,33 +1,22 @@
-import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
+import { Theme, makeStyles } from "@material-ui/core/styles";
+import { Card } from "t9/apps/main/components/card";
+import { EmptyCard } from "t9/apps/main/components/empty-card";
 import Grid from "@material-ui/core/Grid";
 import { Project } from "t9/types/fullstack";
 import React from "react";
-import Skeleton from "@material-ui/lab/Skeleton/Skeleton";
 import Typography from "@material-ui/core/Typography";
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-      paddingBottom: theme.spacing(4),
-    },
-    header: {
-      padding: theme.spacing(2),
-      paddingBottom: theme.spacing(6),
-      textAlign: "center",
-    },
-    cardRoot: {},
-    media: {
-      height: 300,
-    },
-  }),
-);
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    flexGrow: 1,
+    paddingBottom: theme.spacing(4),
+  },
+  header: {
+    padding: theme.spacing(2),
+    paddingBottom: theme.spacing(6),
+    textAlign: "center",
+  },
+}));
 
 export const Catalog = (props: { projectsList: Project[] | null }) => {
   const classes = useStyles();
@@ -39,69 +28,19 @@ export const Catalog = (props: { projectsList: Project[] | null }) => {
       </Typography>
       <Grid container className={classes.root} spacing={4}>
         {props.projectsList
-          ? props.projectsList.map((project) => (
-              <Grid
-                key={`project-${project.slug}`}
-                alignItems="center"
-                alignContent="center"
-                item
-                xs={12}
-                md={6}
-                lg={4}
-              >
-                <Card className={classes.cardRoot}>
-                  <CardActionArea>
-                    <CardMedia
-                      className={classes.media}
-                      image={project.image}
-                      title={project.title}
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        {project.title}
-                      </Typography>
-                      <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        component="p"
-                      >
-                        {project.description}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                  <CardActions>
-                    <Button
-                      size="small"
-                      color="primary"
-                      href={`https://github.com/${project.githubURI}`}
-                    >
-                      Github Repository
-                    </Button>
-                  </CardActions>
-                </Card>
+          ? props.projectsList.map((project: Project) => (
+              <Grid key={`project-${project.slug}`} item xs={12} md={6} lg={4}>
+                <Card
+                  title={project.title}
+                  image={project.image}
+                  githubURI={project.githubURI}
+                  description={project.description}
+                />
               </Grid>
             ))
           : [1, 2, 3].map((id) => (
               <Grid key={`project-${id}`} item>
-                <Card className={classes.cardRoot}>
-                  <CardActionArea>
-                    <Skeleton
-                      animation="wave"
-                      variant="rect"
-                      className={classes.media}
-                    />
-                    <CardContent>
-                      <>
-                        <Skeleton
-                          animation="wave"
-                          height={10}
-                          style={{ marginBottom: 6 }}
-                        />
-                        <Skeleton animation="wave" height={10} width="80%" />
-                      </>
-                    </CardContent>
-                  </CardActionArea>
-                </Card>
+                <EmptyCard />
               </Grid>
             ))}
       </Grid>
