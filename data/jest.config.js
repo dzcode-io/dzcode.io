802a7de8326e7b599226ccf3caf1d1d99a78152d
Zakaria Mansouri <zaqo.cs@outlook.com>
2020-10-12 19:05:01 +0400

    🧪 setup test config

---
+++ data/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  testEnvironment: "node",
+  collectCoverageFrom: ["src/**/*.{ts,tsx}"],
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 80,
+  //     functions: 80,
+  //     lines: 80,
+  //     statements: 80,
+  //   },
+  // },
+  transform: {
+    "^.+\\.(ts|tsx)?$": "ts-jest",
+  },
+  coveragePathIgnorePatterns: ["node_modules", "dist"],
+};
