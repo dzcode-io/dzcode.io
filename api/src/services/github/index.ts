8c701264b3d56d2b5b23cb13362996564cbe2470
Adam Ay <adam.ayuda.1@gmail.com>
2020-10-24 19:59:05 +0400

    Api new github endpoint: listPullRequestsByRepo

--- api/src/services/github/index.ts
+++ api/src/services/github/index.ts
@@ -16,3 +16,22 @@ export const listOrganizationRepositories = async ({
     return null;
   }
 };
+
+export const listPullRequests = async ({
+  owner,
+  repo,
+}: {
+  owner: string;
+  repo: string;
+}) => {
+  try {
+    const response = await axios.get(
+      `https://api.github.com/repos/${owner}/${repo}/pulls`,
+    );
+
+    return response.data;
+  } catch (error) {
+    console.log("listPullRequests ERROR =>", error.response.data);
+    return null;
+  }
+};
