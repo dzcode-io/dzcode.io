5df6f89e52e48a87116143b30f67cc2c1a64716a
Zakaria Mansouri <zaqo.cs@outlook.com>
2020-12-28 21:03:31 +0400

    added loading component

--- web/src/components/loading/index.tsx
+++ web/src/components/loading/index.tsx
@@ -1,1 +1,26 @@
-export const Loading = () => <div>Loading</div>;
+import { FC } from "react";
+import logoSquare from "src/assets/svg/logo-wide.svg";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    textAlign: "center",
+  },
+  image: {
+    display: "inline-block",
+    marginTop: theme.spacing(10),
+    marginBottom: theme.spacing(10),
+    maxWidth: "400px",
+  },
+}));
+
+export const Loading: FC = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <img src={logoSquare} alt="DzCode i/o" className={classes.image} />
+    </div>
+  );
+};
+
+export default Loading;
