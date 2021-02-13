571796cfe2c4e41d3902001bd22ac3bccb555302
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-02-13 18:40:42 +0100

    add react-native-help-create project 🐱‍👤

---
+++ data/models/projects/React_Native_Help_Create/info.js
@@ -0,0 +1,6 @@
+{
+  "title": "react-native-help-create",
+  "description": "This command line helps you create components, screens and even redux implementation for your react native project.",
+  "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Npm-logo.svg/1280px-Npm-logo.svg.png",
+  "githubURI": "Omar-Belghaouti/react-native-help-create"
+}
