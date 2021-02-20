9a5fe75d0560350f46e0029493fa771e3ee10a67
Zakaria Mansouri <zaqo.cs@outlook.com>
2021-02-21 03:00:16 +0400

    update direction based on language selected

--- web/src/apps/main/redux/reducers/settings/index.ts
+++ web/src/apps/main/redux/reducers/settings/index.ts
@@ -1,5 +1,14 @@
 import { Language } from "@dzcode.io/common/dist/types";
 
+const setBodyDir = (lang: Language) => {
+  if (lang === "ar") {
+    document.body.setAttribute("dir", "rtl");
+  } else {
+    document.body.setAttribute("dir", "ltr");
+  }
+  return lang;
+};
+
 export interface SettingsState {
   darkMode: boolean;
   lang: Language;
@@ -8,7 +17,7 @@ export interface SettingsState {
 export const settings = (
   state: SettingsState = {
     darkMode: localStorage.getItem("darkMode") !== "off",
-    lang: (localStorage.getItem("lang") as Language) || "en",
+    lang: setBodyDir((localStorage.getItem("lang") as Language) || "en"),
   },
   action: {
     type: string;
@@ -21,6 +30,7 @@ export const settings = (
       return { ...state, ...action.payload };
     case "UPDATE_LANGUAGE":
       localStorage.setItem("lang", action.payload.lang);
+      setBodyDir(action.payload.lang);
       return { ...state, ...action.payload };
     default:
       return state;
