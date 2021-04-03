00322be5f6d8623fe1970eee40a4daf0541e455c
Woosik Kim <iam.woosik.kim@gmail.com>
2021-04-03 17:37:16 +0900

    Create root controller

--- api/src/routes/api/index.ts
+++ api/src/routes/api/index.ts
@@ -1,10 +1,12 @@
 import express, { Router } from "express";
 
 import contributors from "./contributors";
+import { getRoot } from "../../controllers/etc";
 import github from "./github";
 
 const router: Router = express.Router();
 
+router.get("/", getRoot);
 router.use("/github", github);
 router.use("/contributors", contributors);
 
