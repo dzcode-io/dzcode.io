9d3038a711610ef54bccfd689d163ce09a81f160
Zakaria Mansouri <zakman.dev@gmail.com>
2021-04-04 23:08:08 +0200

    loaded old endpoints & put new ones under /v2

--- api/src/app/index.ts
+++ api/src/app/index.ts
@@ -6,6 +6,7 @@ import { Application } from "express";
 import { ConfigService } from "../config/service";
 import Container from "typedi";
 import { ContributorController } from "../contributor/controller";
+import router from "./routes/api";
 
 // Use typedi container
 useContainer(Container);
@@ -13,7 +14,11 @@ useContainer(Container);
 // Create the app
 const app: Application = createExpressServer({
   controllers: [ContributorController],
+  routePrefix: "v2",
 });
+
+// Load old code, temporarily until we migrate all endpoints
+app.use("/", router);
 
 const port = Container.get(ConfigService).env().PORT;
 
