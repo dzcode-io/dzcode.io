d5424989ce54638065072081460a6a01d6a2d458
Zakaria Mansouri <zakman.dev@gmail.com>
2021-04-05 17:29:31 +0200

    added error handeler and refactored middlewares

--- api/src/app/index.ts
+++ api/src/app/index.ts
@@ -6,6 +6,9 @@ import { Application } from "express";
 import { ConfigService } from "../config/service";
 import Container from "typedi";
 import { ContributorController } from "../contributor/controller";
+import { ErrorMiddleware } from "./middlewares/error";
+import { LoggerMiddleware } from "./middlewares/logger";
+import { SecurityMiddleware } from "./middlewares/security";
 import router from "./routes/api";
 
 // Use typedi container
@@ -14,7 +17,15 @@ useContainer(Container);
 // Create the app
 const app: Application = createExpressServer({
   controllers: [ContributorController],
+  middlewares: [
+    // middlewares:
+    SecurityMiddleware,
+    ErrorMiddleware,
+    LoggerMiddleware,
+  ],
   routePrefix: "v2",
+  defaultErrorHandler: false,
+  cors: Container.get(SecurityMiddleware).cors(),
 });
 
 // Load old code, temporarily until we migrate all endpoints
---
+++ api/src/app/middlewares/error.ts
@@ -0,0 +1,34 @@
+import {
+  ExpressErrorMiddlewareInterface,
+  Middleware,
+} from "routing-controllers";
+
+import { ErrorRequestHandler } from "express";
+import { GeneralResponse } from "../types";
+import { Service } from "typedi";
+
+@Service()
+@Middleware({ type: "after" })
+export class ErrorMiddleware implements ExpressErrorMiddlewareInterface {
+  error: ErrorRequestHandler<never, GeneralResponse, unknown> = (
+    err,
+    req,
+    res,
+    next,
+  ) => {
+    // Logs error
+    console.log("🚩 Internal Server Error");
+    console.log(err);
+
+    // Skip if headers are already sent
+    if (res.headersSent) {
+      return next(err);
+    }
+
+    // return a general error response
+    res.status(500).json({
+      code: 500,
+      msg: err.message,
+    });
+  };
+}
