7ab7c508b365ad5d75c18d9c34e9ffbbfce682a4
Zakaria Mansouri <zakman.dev@gmail.com>
2021-04-05 20:13:14 +0200

    100% unit test coverage for ContributorController

---
+++ api/test/mocks.ts
@@ -0,0 +1,25 @@
+import { GithubUser } from "@dzcode.io/common/dist/types";
+
+export const githubUserMock: GithubUser = {
+  avatar_url: "avatar_url", // eslint-disable-line camelcase
+  html_url: "html_url", // eslint-disable-line camelcase
+  id: "id",
+  login: "login",
+  type: "type",
+};
+
+export const githubUserMock2: GithubUser = {
+  avatar_url: "avatar_url2", // eslint-disable-line camelcase
+  html_url: "html_url2", // eslint-disable-line camelcase
+  id: "id2",
+  login: "login2",
+  type: "type2",
+};
+
+export const githubUserMock3: GithubUser = {
+  avatar_url: "avatar_url3", // eslint-disable-line camelcase
+  html_url: "html_url3", // eslint-disable-line camelcase
+  id: "id3",
+  login: "login3",
+  type: "type3",
+};
