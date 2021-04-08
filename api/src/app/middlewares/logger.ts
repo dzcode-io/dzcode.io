65f0c9c9544931d7c0aef1881ca80a811dc4a605
Zakaria Mansouri <zakman.dev@gmail.com>
2021-04-08 21:34:16 +0200

    fixed linting issues

--- api/src/app/middlewares/logger.ts
+++ api/src/app/middlewares/logger.ts
@@ -6,14 +6,18 @@ import { Service } from "typedi";
 @Service()
 @Middleware({ type: "after" })
 export class LoggerMiddleware implements ExpressMiddlewareInterface {
-  constructor(private loggerService: LoggerService) { }
+  constructor(private loggerService: LoggerService) {}
 
   use: RequestHandler = (req, res, next) => {
     let logLevel: LogLevel = "info";
     const { statusCode } = res;
-    if (statusCode < 100 && statusCode >= 400) { logLevel = "error" }
+    if (statusCode < 100 && statusCode >= 400) {
+      logLevel = "error";
+    }
 
-    this.loggerService.log(logLevel, { message: `${res.statusCode} ${req.method} ${req.url}` })
+    this.loggerService.log(logLevel, {
+      message: `${res.statusCode} ${req.method} ${req.url}`,
+    });
     next();
-  }
+  };
 }
--- api/src/logger/service.ts
+++ api/src/logger/service.ts
@@ -7,35 +7,54 @@ export class LoggerService {
     this.logger = winston.createLogger({
       level: "info",
       format: winston.format.json(),
-      transports: [new winston.transports.Console({ format: winston.format.combine(winston.format.colorize(), winston.format.simple()) })],
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.colorize(),
+            winston.format.simple(),
+          ),
+        }),
+      ],
     });
   }
 
   public log(level: LogLevel, logInfo: LogObject) {
-    this.logger.log(level, logInfo.message, { ...logInfo, message: undefined })
+    this.logger.log(level, logInfo.message, { ...logInfo, message: undefined });
   }
 
   public info(logInfo: LogObject) {
-    this.logger.log("info", logInfo.message, { ...logInfo, message: undefined })
+    this.logger.log("info", logInfo.message, {
+      ...logInfo,
+      message: undefined,
+    });
   }
 
   public error(logInfo: LogObject) {
-    this.logger.log("error", logInfo.message, { ...logInfo, message: undefined })
+    this.logger.log("error", logInfo.message, {
+      ...logInfo,
+      message: undefined,
+    });
   }
 
   public debug(logInfo: LogObject) {
-    this.logger.log("debug", logInfo.message, { ...logInfo, message: undefined })
+    this.logger.log("debug", logInfo.message, {
+      ...logInfo,
+      message: undefined,
+    });
   }
 
   public warn(logInfo: LogObject) {
-    this.logger.log("warn", logInfo.message, { ...logInfo, message: undefined })
+    this.logger.log("warn", logInfo.message, {
+      ...logInfo,
+      message: undefined,
+    });
   }
 
   private logger;
 }
 
-export type LogLevel = "info" | "error" | "debug" | "warn"
+export type LogLevel = "info" | "error" | "debug" | "warn";
 type LogObject = {
-  message: string,
-  [key: string]: unknown
-}
+  message: string;
+  [key: string]: unknown;
+};
