72a59b67032fdcebcd9a01a21ee6cf6e18d226b9
Zakaria Mansouri <zakman.dev@gmail.com>
2021-04-18 14:27:25 +0200

    creates new store intead of using the common one

--- web/src/apps/main/components/footer/footer.spec.tsx
+++ web/src/apps/main/components/footer/footer.spec.tsx
@@ -1,11 +1,12 @@
 import { Footer } from ".";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
-import { mainStore } from "src/apps/main/redux";
+import { createMainStore } from "src/apps/main/redux";
 import { render } from "@testing-library/react";
 
 describe("components/footer/footer.spec.tsx", () => {
   test("should render properly", () => {
+    const mainStore = createMainStore();
     const { container } = render(
       <Provider store={mainStore}>
         <Router>
