84f7c50ffd0fd795a486f1707d801ccf58087077
Zakaria Mansouri <zakman.dev@gmail.com>
2021-06-12 13:25:07 +0200

    fixed failing unit test for github service

--- api/src/github/service.spec.ts
+++ api/src/github/service.spec.ts
@@ -1,8 +1,10 @@
 import { GeneralGithubQuery, ListContributorsResponse } from "./types";
 
 import Axios from "axios";
+import { FetchService } from "../fetch/service";
 import { GithubService } from "./service";
 import { githubUserMock } from "../../test/mocks";
+import { mock } from "jest-mock-extended";
 
 jest.mock("axios");
 const mockedAxios = Axios as jest.Mocked<typeof Axios>;
@@ -20,9 +22,11 @@ describe("GithubService", () => {
     },
   ];
 
+  const fetchService = mock<FetchService>();
+
   it("should throw error when api call fails", async () => {
     mockedAxios.get.mockRejectedValue({ meg: "service down" });
-    const githubService = new GithubService();
+    const githubService = new GithubService(fetchService);
     let errorThrown = false;
     try {
       await githubService.listContributors(githubQuery);
@@ -34,7 +38,7 @@ describe("GithubService", () => {
 
   it("should return list of contributors when api call succeed", async () => {
     mockedAxios.get.mockResolvedValue({ data: contributorsMock });
-    const githubService = new GithubService();
+    const githubService = new GithubService(fetchService);
     const contributors = await githubService.listContributors(githubQuery);
 
     expect(contributors).toMatchObject([githubUserMock]);
