0698f5c6652ae80b128027f3c2a190f0426e1093
Zakaria Mansouri <zakman.dev@gmail.com>
2021-06-13 14:56:06 +0200

    filter contributions by project, label, language

--- api/src/contribution/controller.ts
+++ api/src/contribution/controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Get } from "routing-controllers";
+import { Controller, Get, QueryParams } from "routing-controllers";
+import {
+  GetContributionsQueryDto,
+  GetContributionsResponseDto,
+} from "@dzcode.io/common/dist/types/api-responses";
 import { OpenAPI, ResponseSchema } from "routing-controllers-openapi";
 import { ContributionRepository } from "./repository";
-import { GetContributionsResponseDto } from "@dzcode.io/common/dist/types/api-responses";
 import { Service } from "typedi";
 
 @Service()
@@ -17,8 +20,22 @@ export class ContributionController {
       "Return a list of contributions for all projects listed in dzcode.io",
   })
   @ResponseSchema(GetContributionsResponseDto)
-  public async getContributions(): Promise<GetContributionsResponseDto> {
-    const { contributions, filters } = await this.contributionRepository.find();
+  public async getContributions(
+    @QueryParams() { labels, languages, projects }: GetContributionsQueryDto,
+  ): Promise<GetContributionsResponseDto> {
+    const { contributions, filters } = await this.contributionRepository.find(
+      (contribution) =>
+        (labels.length === 0 ||
+          labels.some((label) => contribution.labels.includes(label))) &&
+        (languages.length === 0 ||
+          languages.some((language) =>
+            contribution.languages.includes(language),
+          )) &&
+        (projects.length === 0 ||
+          projects.some((project) => {
+            return contribution.project.id === project;
+          })),
+    );
     return {
       contributions,
       filters,
