ae438451153e2b264c6abb55a571496dddc4273c
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-07-23 22:34:16 +0100

    Using theme in all screens

--- mobile/src/screens/Articles/ui/ArticlesUI.tsx
+++ mobile/src/screens/Articles/ui/ArticlesUI.tsx
@@ -1,6 +1,9 @@
 // import react native
 import React, { FC } from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
+
+// import react native paper
+import { Text, useTheme } from "react-native-paper";
 
 // import styles
 import { globalStyles } from "../../../styles";
@@ -13,12 +16,19 @@ interface ArticlesUIProps {}
 
 // export Articles UI
 const ArticlesUI: FC<ArticlesUIProps> = ({}): JSX.Element => {
+  // use theme
+  const theme = useTheme();
+
   return (
     // main view
-    <View style={globalStyles.mainView}>
+    <View
+      style={[globalStyles.mainView, { backgroundColor: theme.colors.primary }]}
+    >
       {/* center view */}
       <View style={globalStyles.centerView}>
-        <Text style={globalStyles.titleText}>Articles UI created!</Text>
+        <Text style={[globalStyles.titleText, { color: theme.colors.text }]}>
+          Articles UI created!
+        </Text>
       </View>
     </View>
   );
