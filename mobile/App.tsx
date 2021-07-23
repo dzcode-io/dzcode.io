5eac3ff101f76ec8bf43a704dc37f3be34aaa780
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-07-23 18:27:41 +0100

    Applying react native paper as a provider for material design

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,17 +1,37 @@
 // import react native
 import React, { FC } from "react";
 
+// import react native paper
+import { Provider as PaperProvider, DefaultTheme } from "react-native-paper";
+
 // import navigation container
 import { NavigationContainer } from "@react-navigation/native";
 
 // import screens
 import Navigation from "./src/screens/Navigation";
 
+// import colors
+import Colors from "./src/styles/Colors";
+
+// define default theme
+const theme = {
+  ...DefaultTheme,
+  dark: true,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: Colors.primary,
+    accent: Colors.accent,
+    background: Colors.primary,
+  },
+};
+
 const App: FC = (): JSX.Element => {
   return (
-    <NavigationContainer>
-      <Navigation />
-    </NavigationContainer>
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <Navigation />
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
 export default App;
