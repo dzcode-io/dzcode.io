1c9dc686eedcc3a5a636f095c1670524a1ca9020
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-08-06 23:33:06 +0100

    implementing contribute card

--- mobile/src/components/shared/index.ts
+++ mobile/src/components/shared/index.ts
@@ -1,3 +1,4 @@
 // export shared components
 export { default as AppBar } from "./app-bar";
 export { default as DrawerContent } from "./drawer-content";
+export { default as CardItem } from "./card-item";
