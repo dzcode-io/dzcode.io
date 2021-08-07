f37cb7730dfbed093ec2f93d2366e6c66e98f48e
Zakaria Mansouri <zakman.dev@gmail.com>
2021-08-07 20:42:08 +0200

    removed redundant default React import

--- web/src/apps/main/entry/index.tsx
+++ web/src/apps/main/entry/index.tsx
@@ -1,6 +1,5 @@
 import { App } from "./app";
 import { Provider } from "react-redux";
-import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { mainStore } from "src/apps/main/redux";
 import { render } from "react-dom";
