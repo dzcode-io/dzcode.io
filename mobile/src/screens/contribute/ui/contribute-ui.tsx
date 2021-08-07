cdc470696003c93ae1ce92d35cbd4c165e2aec6c
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-08-07 08:28:04 +0100

    filtering contributions on chip press

--- mobile/src/screens/contribute/ui/contribute-ui.tsx
+++ mobile/src/screens/contribute/ui/contribute-ui.tsx
@@ -1,10 +1,9 @@
 import {} from "../functions";
-import { FlatList, View, RefreshControl } from "react-native";
-import React, { FC, useState, useEffect, useCallback } from "react";
+import { FlatList, View, Linking } from "react-native";
+import React, { FC, useCallback, useEffect, useState } from "react";
 import { CardItem } from "../../../components/shared";
 import { ContributionsResponse } from "../../../api/interfaces";
 import { DZCodeLoading } from "../../../components/shared";
-import { calculateDate } from "../../../utils/functions";
 import { getContributes } from "../../../api/requests";
 import { globalStyles } from "../../../styles";
 
@@ -16,19 +15,25 @@ const ContributeUI: FC = (): JSX.Element => {
   // use refreshing state
   const [refreshing, setRefreshing] = useState(false);
 
+  // use is loading state
+  const [isLoading, setIsLoading] = useState(false);
+
   // get contributions from backend
-  const getContributions = useCallback(async () => {
-    // set refreshing state
-    setRefreshing(true);
-    // get contributions from backend
-    const contributesResponse = await getContributes();
-    if (contributesResponse) {
-      // set contributions state
-      setContributions(contributesResponse.contributions);
-    }
-    // set refreshing state
-    setRefreshing(false);
-  }, []);
+  const getContributions = useCallback(
+    async (projects: string[] = [], languages: string[] = [], labels: string[] = []) => {
+      // set loading state
+      setIsLoading(true);
+      // get contributions from backend
+      const contributesResponse = await getContributes(projects, languages, labels);
+      if (contributesResponse) {
+        // set contributions state
+        setContributions(contributesResponse.contributions);
+      }
+      // set loading state
+      setIsLoading(false);
+    },
+    [],
+  );
 
   // use effect on component did mount
   useEffect(() => {
@@ -38,40 +43,55 @@ const ContributeUI: FC = (): JSX.Element => {
   return (
     // main view
     <View style={globalStyles.mainView}>
-      {/* center view */}
-      <View style={globalStyles.centerView}>
-        {/* Loading */}
-        <DZCodeLoading style={{ position: "absolute" }} />
-        {/* Cards */}
-        {contributions.length > 0 && (
-          <FlatList
-            data={contributions}
-            onRefresh={async () => {
-              setRefreshing(true);
-              const contributesResponse = await getContributes();
-              if (contributesResponse) {
-                setContributions(contributesResponse.contributions);
-              }
-              setRefreshing(false);
-            }}
-            refreshing={refreshing}
-            keyExtractor={(item, index) => `${index}-${item?.id}`}
-            renderItem={({ item }) => (
-              <CardItem
-                title={item.title}
-                subtitle={item.project.name}
-                labels={[...item.labels, ...item.languages]}
-                type={item.type}
-                createdAt={item.createdAt}
-                commentsCount={item.commentsCount}
-                onPress={() => {
-                  console.log(calculateDate(new Date(item.createdAt), new Date()));
-                }}
-              />
-            )}
-          />
-        )}
-      </View>
+      {/* Loading */}
+      {isLoading && (
+        <View style={globalStyles.centerView}>
+          <DZCodeLoading style={{ position: "absolute" }} />
+        </View>
+      )}
+      {/* Cards */}
+      {contributions.length > 0 && (
+        <FlatList
+          data={contributions}
+          onRefresh={async () => {
+            setRefreshing(true);
+            const contributesResponse = await getContributes();
+            if (contributesResponse) {
+              setContributions(contributesResponse.contributions);
+            }
+            setRefreshing(false);
+          }}
+          refreshing={refreshing}
+          keyExtractor={(item, index) => `${index}-${item?.id}`}
+          renderItem={({ item }) => (
+            <CardItem
+              title={item.title}
+              subtitle={item.project.name}
+              labels={[...item.labels, ...item.languages]}
+              type={item.type}
+              createdAt={item.createdAt}
+              commentsCount={item.commentsCount}
+              onChipPress={async (i) => {
+                setContributions([]);
+                if (item.labels.includes(i)) {
+                  await getContributions([], [], [i]);
+                  return;
+                }
+                if (item.languages.includes(i)) {
+                  await getContributions([], [i], []);
+                }
+              }}
+              onPress={() => {
+                try {
+                  Linking.openURL(item.url);
+                } catch {
+                  alert("Can't open browser");
+                }
+              }}
+            />
+          )}
+        />
+      )}
     </View>
   );
 };
