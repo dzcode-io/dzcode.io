4d7170e80d68254412397b43b569b9be290d76f2
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-08-07 23:15:33 +0100

    deleting contribute test

--- mobile/__tests__/contribute-test.tsx
+++
@@ -1,7 +0,0 @@
-import Contribute from "../src/screens/contribute/ui/contribute-ui";
-import React from "react";
-import renderer from "react-test-renderer";
-
-it("renders correctly", async () => {
-  renderer.create(<Contribute />);
-});
