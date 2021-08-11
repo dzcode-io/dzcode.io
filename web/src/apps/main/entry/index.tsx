7f85943544925c16a5e37fb56a4318bcb65b3ad4
Zakaria Mansouri <zakman.dev@gmail.com>
2021-08-11 21:09:00 +0200

    Setup Sentry for error logging

--- web/src/apps/main/entry/index.tsx
+++ web/src/apps/main/entry/index.tsx
@@ -1,8 +1,16 @@
+import * as Sentry from "@sentry/browser";
 import { App } from "./app";
+import { Integrations } from "@sentry/tracing";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import { mainStore } from "src/apps/main/redux";
 import { render } from "react-dom";
+
+Sentry.init({
+  dsn: "https://b42ee25f06c4416dac83de4ab8772e26@o953637.ingest.sentry.io/5902610",
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 render(
   <Provider store={mainStore}>
