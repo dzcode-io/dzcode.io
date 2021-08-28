82afd8b8a83e3fd70303e6207b8372534bdd0acc
Zakaria Mansouri <zakman.dev@gmail.com>
2021-08-28 20:25:54 +0200

    setup redux and got theme from redux state

--- mobile/src/components/shared/app-bar.tsx
+++ mobile/src/components/shared/app-bar.tsx
@@ -1,8 +1,5 @@
-import { Appbar, Switch, Text, useTheme } from "react-native-paper";
-import React, { FC, useContext } from "react";
-import Colors from "../../styles/colors";
-import { PrefrencesContext } from "../../utils/constants";
-import { drawerStyles } from "../../styles";
+import { Appbar, useTheme } from "react-native-paper";
+import React, { FC } from "react";
 
 // define Appbar props interface
 interface AppbarProps {
@@ -15,23 +12,12 @@ const AppBar: FC<AppbarProps> = ({ title, openDrawer }): JSX.Element => {
   // use theme
   const theme = useTheme();
 
-  // use theme context
-  const { toggleTheme, isThemeDark } = useContext(PrefrencesContext);
-
   return (
     // Appbar header
     <Appbar.Header theme={{ colors: { primary: theme?.colors.surface } }}>
       {/* Appbar content */}
       <Appbar.Action icon="menu" onPress={() => openDrawer()} />
       <Appbar.Content title={title} />
-      {/* Dark theme switch */}
-      <Text>Dark</Text>
-      <Switch
-        onValueChange={() => toggleTheme()}
-        color={Colors.accent}
-        value={isThemeDark}
-        style={drawerStyles.switchView}
-      />
     </Appbar.Header>
   );
 };
