fc3ce9c01cfedce588c26beb3a87608fd953bea4
Zakaria Mansouri <zakman.dev@gmail.com>
2021-08-28 21:02:21 +0200

    added init action that setup a theme change listener

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable unicorn/filename-case */
 import * as Sentry from "sentry-expo";
+import { Dispatch, StateInterface, mainStore } from "./src/redux";
 import {
   Theme as NT,
   NavigationContainer,
@@ -11,14 +12,14 @@ import {
   DefaultTheme as PaperDefaultTheme,
   Provider as PaperProvider,
 } from "react-native-paper";
-import { Provider, useSelector } from "react-redux";
-import React, { FC } from "react";
-import { StateInterface, mainStore } from "./src/redux";
+import { Provider, useDispatch, useSelector } from "react-redux";
+import React, { FC, useEffect } from "react";
 import Colors from "./src/styles/colors";
 import { GeneralState } from "./src/redux/reducers/general";
 import Navigation from "./src/screens/navigation";
 import { Theme as PT } from "react-native-paper/lib/typescript/types";
 import { getEnv } from "./src/utils/env";
+import { init } from "./src/redux/actions/general";
 
 const env = getEnv();
 
@@ -72,6 +73,11 @@ const darkTheme = {
 
 const App: FC = () => {
   const { theme: themeName } = useSelector<StateInterface, GeneralState>((state) => state.general);
+  const dispatch = useDispatch<Dispatch<GeneralState>>();
+
+  useEffect(() => {
+    dispatch(init());
+  }, []);
 
   const themes = {
     dark: darkTheme,
---
+++ mobile/src/redux/actions/general/index.ts
@@ -0,0 +1,19 @@
+import { GeneralState, getThemeFromSystem } from "../../reducers/general";
+import { Appearance } from "react-native";
+import { SettingsState } from "../../reducers/settings";
+import { ThunkResult } from "../..";
+
+/**
+ * Initiate some watchers and load state from Async Storage
+ */
+export const init = (): ThunkResult<SettingsState | GeneralState> => async (dispatch, getState) => {
+  Appearance.addChangeListener(({ colorScheme }) => {
+    const theme = getThemeFromSystem(colorScheme);
+    const currentTheme = getState().general.theme;
+    if (theme === currentTheme) return;
+    dispatch({
+      type: "UPDATE_GENERAL",
+      payload: { theme },
+    });
+  });
+};
