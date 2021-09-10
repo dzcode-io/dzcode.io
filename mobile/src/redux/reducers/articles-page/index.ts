4eead59dcdc126f2dd64e39937404470ee26a500
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-09-10 11:14:46 +0100

    adding articles page reducer

---
+++ mobile/src/redux/reducers/articles-page/index.ts
@@ -0,0 +1,23 @@
+import { Article } from "../../../.common/types";
+import { Action } from "../..";
+
+export interface ArticlesPageState {
+  articles: Article[] | null;
+  refreshing: boolean;
+}
+
+export const articlesPage = (
+  state: ArticlesPageState = {
+    articles: null,
+    refreshing: false,
+  },
+  action: Action<ArticlesPageState>,
+) => {
+  switch (action.type) {
+    case "UPDATE_ARTICLES_PAGE":
+      return { ...state, ...action.payload };
+
+    default:
+      return state;
+  }
+};
