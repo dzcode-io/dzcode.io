4c68effa4ec4ca38e69e17ad7e43178600dbff21
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-09-12 00:00:12 +0100

    adding contributions empty state

--- mobile/src/screens/contribute/index.tsx
+++ mobile/src/screens/contribute/index.tsx
@@ -1,6 +1,6 @@
-import { Checkbox, FAB, List } from "react-native-paper";
+import { Checkbox, FAB, List, Text } from "react-native-paper";
 import { Dispatch, StateInterface } from "../../redux";
-import { FlatList, Linking, ScrollView, View } from "react-native";
+import { FlatList, Linking, ScrollView, View, Image } from "react-native";
 import React, { FC, useEffect, useState } from "react";
 import { globalStyles } from "../../styles";
 import { contributeStyles } from "./styles";
@@ -27,36 +27,46 @@ const ContributeScreen: FC = () => {
     <View style={globalStyles.mainView}>
       {contributions ? (
         // Cards
-        <FlatList
-          style={contributeStyles.listView}
-          data={contributions}
-          onRefresh={() => {
-            dispatch(fetchContributions());
-          }}
-          refreshing={refreshing}
-          keyExtractor={(item, index) => `item-${index}`}
-          renderItem={({ item }) => (
-            <CardItem
-              title={item.title}
-              subtitle={item.project.name}
-              labels={[...item.labels, ...item.languages]}
-              type={item.type}
-              createdAt={item.createdAt}
-              commentsCount={item.commentsCount}
-              onChipPress={async (optionName) => {
-                const filterName = item.labels.includes(optionName) ? "labels" : "languages";
-                dispatch(updateFilterValue(filterName, optionName, true, true, true));
-              }}
-              onPress={() => {
-                try {
-                  Linking.openURL(item.url);
-                } catch {
-                  alert("Can't open browser");
-                }
-              }}
+        contributions.length > 0 ? (
+          <FlatList
+            style={contributeStyles.listView}
+            data={contributions}
+            onRefresh={() => {
+              dispatch(fetchContributions());
+            }}
+            refreshing={refreshing}
+            keyExtractor={(item, index) => `item-${index}`}
+            renderItem={({ item }) => (
+              <CardItem
+                title={item.title}
+                subtitle={item.project.name}
+                labels={[...item.labels, ...item.languages]}
+                type={item.type}
+                createdAt={item.createdAt}
+                commentsCount={item.commentsCount}
+                onChipPress={async (optionName) => {
+                  const filterName = item.labels.includes(optionName) ? "labels" : "languages";
+                  dispatch(updateFilterValue(filterName, optionName, true, true, true));
+                }}
+                onPress={() => {
+                  try {
+                    Linking.openURL(item.url);
+                  } catch {
+                    alert("Can't open browser");
+                  }
+                }}
+              />
+            )}
+          />
+        ) : (
+          <View style={globalStyles.centerView}>
+            <Image
+              source={require("../../assets/png/info.png")}
+              style={contributeStyles.emptyStateLogo}
             />
-          )}
-        />
+            <Text style={contributeStyles.emptyStateText}>There is no contribution</Text>
+          </View>
+        )
       ) : (
         // Loading indicator
         <View style={globalStyles.centerView}>
--- mobile/src/screens/contribute/styles.ts
+++ mobile/src/screens/contribute/styles.ts
@@ -1,9 +1,24 @@
-import { MEDIUM_MARGIN_SIZE } from "../../utils/constants";
+import {
+  MEDIUM_LOGO_HEIGHT,
+  MEDIUM_LOGO_WIDTH,
+  MEDIUM_MARGIN_SIZE,
+  MEDIUM_TEXT_SIZE,
+} from "../../utils/constants";
 import { StyleSheet } from "react-native";
 
 export const contributeStyles = StyleSheet.create({
   dzcodeLoading: {
     position: "absolute",
+  },
+  emptyStateLogo: {
+    width: MEDIUM_LOGO_WIDTH,
+    height: MEDIUM_LOGO_HEIGHT,
+  },
+  emptyStateText: {
+    marginTop: MEDIUM_MARGIN_SIZE,
+    marginBottom: MEDIUM_MARGIN_SIZE,
+    textAlign: "center",
+    fontSize: MEDIUM_TEXT_SIZE,
   },
   listView: {
     marginVertical: MEDIUM_MARGIN_SIZE,
