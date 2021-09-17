421a9a7a0ee03de19aedf5bafe332e048b94e148
Zakaria Mansouri <zakman.dev@gmail.com>
2021-09-17 23:31:17 +0200

    used fetchV2 with `articles/list.c.json` endpint

--- mobile/src/utils/fetch/endpoints.ts
+++ mobile/src/utils/fetch/endpoints.ts
@@ -1,6 +1,10 @@
+import { Article } from "../../_common/types";
 import { GetContributionsResponseDto } from "../../_common/types/api-responses";
 
 export interface Endpoints {
+  "data:articles/list.c.json": {
+    response: Article[]; // TODO-ZM: should be: Pick<Article, "title">[] instead
+  };
   "api:v2/Contributions": {
     response: GetContributionsResponseDto;
     query: [string, string][];
