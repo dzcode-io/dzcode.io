8c714d32aad25eabfb7a565fa991cfe19d974ab0
Zakaria Mansouri <zakman.dev@gmail.com>
2021-09-18 22:02:07 +0200

    added two endpoints to fetchV2 & used them in `/Learn` page

--- common/src/types/endpoints.ts
+++ common/src/types/endpoints.ts
@@ -1,16 +1,23 @@
+import { Article, Document } from ".";
 import {
   GetContributionsResponseDto,
   GetContributorsResponseDto,
   GetUserResponseDto,
 } from "./api-responses";
-import { Article } from ".";
 
 export interface Endpoints {
   "data:articles/list.c.json": {
-    response: Article[]; // TODO-ZM: should be: Pick<Article, "title">[] instead
+    response: Article[]; // TODO-ZM: should be: Pick<Article, "title" | "slug">[] instead
   };
   "data:articles/:slug.json": {
     response: Article;
+    params: { slug: string };
+  };
+  "data:documentation/list.c.json": {
+    response: Pick<Document, "title" | "slug">[];
+  };
+  "data:documentation/:slug.json": {
+    response: Document;
     params: { slug: string };
   };
   "api:v2/Contributions": {
