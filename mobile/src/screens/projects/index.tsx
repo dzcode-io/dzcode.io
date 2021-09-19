e37999f85323855276f837ccf7782869eabfad88
Zakaria Mansouri <zakman.dev@gmail.com>
2021-09-19 19:32:24 +0200

    put all screens inside `SafeAreaView` component

--- mobile/src/screens/projects/index.tsx
+++ mobile/src/screens/projects/index.tsx
@@ -1,16 +1,15 @@
 import React, { FC } from "react";
+import { SafeAreaView, View } from "react-native";
 import { Text } from "react-native-paper";
-import { View } from "react-native";
 import { globalStyles } from "../../styles/global";
 
 export const ProjectsScreen: FC = () => {
   return (
-    // main view
-    <View style={globalStyles.mainView}>
+    <SafeAreaView style={globalStyles.mainView}>
       {/* center view */}
       <View style={globalStyles.centerView}>
         <Text style={globalStyles.titleText}>Projects UI created!</Text>
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
