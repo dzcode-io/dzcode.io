fd940ec19c509993b89dc3cbb3b711f51c8420d1
abderrahmane <abderrahmaneMustapha030898@gmail.com>
2021-10-04 23:43:28 +0100

    feat : automatique message detection, extration and then compile them into multiple json files for each language, note that this files is not gonnna be overrwitten

--- web/src/localization/locals/ar.ts
+++
@@ -1,8 +0,0 @@
-const message: { [key: string]: string } = {
-  Learn: "اقرأ",
-  Projects: "مشاريع",
-  Articles: "مقالات",
-  FAQ: "الأسئلة المتداولة",
-};
-
-export default message;
--- web/src/localization/locals/fr.ts
+++
@@ -1,8 +0,0 @@
-const message: { [key: string]: string } = {
-  Learn: "Lire",
-  Projects: "Projets",
-  Articles: "Articles",
-  FAQ: "FAQ",
-};
-
-export default message;
