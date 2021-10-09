57df5c534591e68e1a85b55eb61cb7a95a44f3db
souhaib benbouzid <sohaib.benbouzid@emploitic.com>
2021-10-09 10:52:57 +0100

    header rework

--- web/jest.config.js
+++ web/jest.config.js
@@ -9,6 +9,11 @@ module.exports = {
   //     statements: 80,
   //   },
   // },
+  moduleNameMapper: {
+    "^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/src/__mocks__/file-mock.ts",
+    "^.+\\.(css|sass|scss)$": "<rootDir>/src/__mocks__/style-mock.ts",
+  },
   transform: {
     "^.+\\.(ts|tsx|js|jsx)?$": "ts-jest",
   },
