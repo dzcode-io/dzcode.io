baf855f8b28cf6c5c63b758af704d01c87127e40
Zakaria Mansouri <zakman.dev@gmail.com>
2021-10-17 01:29:42 +0200

    corrected data path when calling `getDataCollection`

--- api/src/team/repository.ts
+++ api/src/team/repository.ts
@@ -11,7 +11,7 @@ import { join } from "path";
 export class TeamRepository {
   constructor(private readonly githubService: GithubService) {
     const projects = getDataCollection<Model<ProjectReferenceEntity, "repositories">>(
-      join(__dirname, "../../../data"),
+      join(__dirname, "../../../../data"),
       "projects-v2",
       "list.json",
     );
