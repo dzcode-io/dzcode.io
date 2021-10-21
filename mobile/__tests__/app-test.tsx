5881f453f0a36ae1a268ab93008d364c63226bc5
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-10-21 18:43:28 +0100

    mocking bottom sheet component

--- mobile/__tests__/app-test.tsx
+++ mobile/__tests__/app-test.tsx
@@ -4,7 +4,22 @@ import renderer from "react-test-renderer";
 
 jest.useFakeTimers();
 
-jest.mock("react-native-reanimated", () => require("react-native-reanimated/mock"));
+const mock = jest.requireMock("react-native-reanimated");
+jest.mock("react-native-reanimated", () => ({
+  ...mock,
+  useSharedValue: jest.fn().mockReturnValue(0),
+  useAnimatedStyle: jest.fn().mockReturnValue({}),
+  useAnimatedScrollHandler: jest.fn().mockReturnValue({}),
+  createAnimatedComponent: (component) => jest.fn().mockReturnValue(component),
+  __reanimatedWorkletInit: jest.fn(),
+  ScrollView: "ScrollView",
+}));
+
+jest.mock("@gorhom/bottom-sheet", () => {
+  require("react-native-reanimated/mock");
+});
+
+global.__reanimatedWorkletInit = jest.fn();
 
 jest.mock("@react-navigation/drawer", () => ({
   createDrawerNavigator: jest.fn().mockReturnValue({
