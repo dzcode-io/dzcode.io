254c753ff2f9824ac9d5a26a31f91576f1cd7828
abderrahmane <abderrahmaneMustapha030898@gmail.com>
2021-11-14 16:07:07 +0100

    remove formatjs support

--- web/src/localization/utils/runner.ts
+++
@@ -1,7 +0,0 @@
-const manageTranslations = require("react-intl-translations-manager").default;
-
-manageTranslations({
-  messagesDirectory: "src/localization/extractions",
-  translationsDirectory: "src/localization/locals",
-  languages: ["ar", "en", "fr"], // any language you need
-});
