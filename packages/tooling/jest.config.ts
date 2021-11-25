5c2a8d59526b7735ebb0763f15292db7c56e17df
Zakaria Mansouri <zakman.dev@gmail.com>
2021-11-25 04:03:36 +0100

    added `test` scripts to `./web`

--- packages/tooling/jest.config.ts
+++ packages/tooling/jest.config.ts
@@ -7,4 +7,10 @@ export default {
   collectCoverageFrom: ["<rootDir>/src/**/*.{ts,tsx,js,jsx}"],
   coveragePathIgnorePatterns: ["<rootDir>/src/_test"],
   testPathIgnorePatterns: ["<rootDir>/(?!src)", "<rootDir>/src/_test"],
+  moduleNameMapper: {
+    "^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/src/__mocks__/file-mock.ts",
+    "^.+\\.(css|sass|scss)$": "<rootDir>/src/__mocks__/style-mock.ts",
+    "^src/(.*)": "<rootDir>/src/$1",
+  },
 } as Config.InitialOptions;
