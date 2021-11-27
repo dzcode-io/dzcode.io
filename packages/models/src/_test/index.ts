f18353cec5eb3e02c0e6d0a5170308569e7619bc
Zakaria Mansouri <zakman.dev@gmail.com>
2021-11-27 05:43:15 +0100

    fixed deps mismatch across the monorepo

--- packages/models/src/_test/index.ts
+++ packages/models/src/_test/index.ts
@@ -1,56 +1,37 @@
-import {
-  OptionalPropertiesOf,
-  RequiredPropertiesOf,
-} from "@dzcode.io/utils/dist/ts";
+import { OptionalPropertiesOf, RequiredPropertiesOf } from "@dzcode.io/utils/dist/ts";
 import { ClassConstructor, plainToClass } from "class-transformer";
 import { validateSync } from "class-validator";
 import { BaseEntity } from "../_base";
 
 export type Cases<T extends object> = [
-  {
-    name: string;
-    input: RequiredPropertiesOf<T>;
-    expectErrors: false;
-  },
-  {
-    name: string;
-    input: Required<T>;
-    expectErrors: false;
-  },
-  {
-    name: string;
-    input: Record<string, never>;
-    expectErrors: boolean;
-  }
+  [string, RequiredPropertiesOf<T>, false],
+  [string, Required<T>, false],
+  [string, Record<string, never>, boolean],
 ];
 
 export const runDTOTestCases = <T extends BaseEntity>(
   entity: ClassConstructor<T>,
   requiredFields: RequiredPropertiesOf<T>,
-  optionalFields: OptionalPropertiesOf<T>
+  optionalFields: OptionalPropertiesOf<T>,
 ): void => {
   const hasNoRequiredFields = Object.keys(requiredFields).length === 0;
   const cases: Cases<T> = [
-    {
-      name: "should match snapshot when providing required fields only",
-      input: requiredFields,
-      expectErrors: false,
-    },
-    {
-      name: "should match snapshot when providing all fields",
-      input: { ...requiredFields, ...optionalFields } as Required<T>,
-      expectErrors: false,
-    },
-    {
-      name: hasNoRequiredFields
+    ["should match snapshot when providing required fields only", requiredFields, false],
+    [
+      "should match snapshot when providing all fields",
+      { ...requiredFields, ...optionalFields } as Required<T>,
+      false,
+    ],
+    [
+      hasNoRequiredFields
         ? "should match snapshot when providing an empty object"
         : "should show an error that matches snapshot when passing empty object",
-      input: {},
-      expectErrors: !hasNoRequiredFields,
-    },
+      {},
+      !hasNoRequiredFields,
+    ],
   ];
 
-  it.each(cases)("$name", ({ input, expectErrors }) => {
+  it.each(cases)("%s", (name, input, expectErrors) => {
     const output = plainToClass(entity, input);
     const errors = validateSync(output);
 
