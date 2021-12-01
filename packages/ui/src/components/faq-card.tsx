9d99824d11f4f538acc3039e5121ac60a518d105
sohaib benbouzid <sohaib.code@gmail.com>
2021-12-01 15:14:19 +0100

    feature: added and `faq card` component

---
+++ packages/ui/src/components/faq-card.tsx
@@ -0,0 +1,46 @@
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Grid,
+  Typography,
+} from "@mui/material";
+
+import { ExpandMore } from "@mui/icons-material";
+import { ReactNode } from "react";
+
+interface FaqCardProps {
+  title: ReactNode;
+  questions: Array<{
+    question: ReactNode;
+    answer: ReactNode;
+  }>;
+}
+
+const FaqCard = ({ title, questions }: FaqCardProps) => {
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Typography
+          variant="h5"
+          component="h2"
+          sx={{ textAlign: { xs: "center", lg: "left" } }}
+        >
+          {title}
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        {questions.map(({ question, answer }, index) => (
+          <Accordion key={`faq-${index}`} variant="outlined">
+            <AccordionSummary expandIcon={<ExpandMore />}>
+              <Typography variant="body1">{question}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>{answer}</AccordionDetails>
+          </Accordion>
+        ))}
+      </Grid>
+    </Grid>
+  );
+};
+
+export default FaqCard;
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,2 +1,4 @@
 export { default as DzCodeThemeProvider } from "./theme-provider";
 export * from "@mui/material";
+
+export { default as FaqCard } from "./components/faq-card";
