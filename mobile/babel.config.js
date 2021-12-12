af652bef74fdb69e5a05eff36443fedca82ba8b2
Zakaria Mansouri <zakman.dev@gmail.com>
2021-12-12 18:01:22 +0100

    get `version` from `bundle-info.json` and display it in mobile navbar

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,5 +1,13 @@
+const { readFileSync } = require("fs");
 const internalIp = require("internal-ip");
 const localIP = internalIp.v4.sync();
+
+let bundleInfo = { version: require("./package.json").version };
+try {
+  bundleInfo = JSON.parse(readFileSync(".bundle-info.json").toString());
+} catch (error) {
+  /**/
+}
 module.exports = function (api) {
   api.cache(true);
   return {
@@ -10,6 +18,7 @@ module.exports = function (api) {
         "transform-define",
         {
           "process.env.LOCAL_API_HOST": localIP,
+          "window.bundleInfo": bundleInfo,
         },
       ],
     ],
