71f2a152f406e337cad0042d5f59a0271bdb025b
Zakaria Mansouri <zakman.dev@gmail.com>
2022-03-20 18:28:09 +0100

    show `TryAgain` compoent when loading error happens in Team page

--- web/src/apps/main/pages/team/catalog/index.tsx
+++ web/src/apps/main/pages/team/catalog/index.tsx
@@ -1,13 +1,13 @@
+import { GetTeamResponseDto } from "@dzcode.io/api/dist/team/types";
 import { Box } from "@dzcode.io/ui/dist/box";
 import { ContributorCard, ContributorSkeleton } from "@dzcode.io/ui/dist/contributor-card";
 import { Grid } from "@dzcode.io/ui/dist/grid";
 import { Typography } from "@dzcode.io/ui/dist/typography";
 import { FC } from "react";
 import { FormattedMessage } from "react-intl";
-import { TeamPageState } from "src/apps/main/redux/reducers/team-page";
 
 interface CatalogProps {
-  teamList: TeamPageState["teamList"];
+  teamList: GetTeamResponseDto["contributors"] | null;
 }
 
 export const Catalog: FC<CatalogProps> = ({ teamList }) => {
@@ -29,7 +29,7 @@ export const Catalog: FC<CatalogProps> = ({ teamList }) => {
               </Box>
             </Grid>
           ))
-        : [1, 2, 3, 4, 5, 6, 7, 8].map((_, index) => (
+        : [1, 2, 3, 4].map((_, index) => (
             <Grid key={index} item xs={12} sm={6} md={4} lg={3}>
               <Box display="flex" justifyContent="center">
                 <ContributorSkeleton />
