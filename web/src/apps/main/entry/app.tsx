cf65ce2d85045724fd92f79e25d21ea2a04ea7b2
Zakaria Mansouri <zakman.dev@gmail.com>
2022-08-05 21:24:37 +0200

    removed react-intl

--- web/src/apps/main/entry/app.tsx
+++ web/src/apps/main/entry/app.tsx
@@ -3,52 +3,16 @@ import "./style.scss";
 import { ErrorBoundary } from "@dzcode.io/ui/dist/error-boundary";
 import Container from "@material-ui/core/Container";
 import { ComponentType, FC, lazy, Suspense, useEffect } from "react";
-import { defineMessages, useIntl } from "react-intl";
-import { Route, Switch, useLocation } from "react-router-dom";
+import { Route, RouteProps, Switch, useLocation } from "react-router-dom";
 import { Footer } from "src/apps/main/components/footer";
 import { Navbar } from "src/apps/main/components/navbar";
 import { Theme } from "src/apps/main/components/theme";
 import { getEnv } from "src/common/utils";
 import { Loading } from "src/components/loading";
-import Localization from "src/localization";
 
-type RouteInterface = {
+interface RouteInterface extends RouteProps {
   import: Promise<{ default: ComponentType }>;
-  exact?: boolean;
-} & ({ translate?: false; path?: string } | { translate: true; path: Record<string, unknown> });
-
-const messages = defineMessages({
-  learn: {
-    id: "learn.path",
-    defaultMessage: "Learn",
-    description: "Learn page route",
-  },
-  projects: {
-    id: "projects.path",
-    defaultMessage: "Projects",
-    description: "Projects page route",
-  },
-  articles: {
-    id: "articles.path",
-    defaultMessage: "Articles",
-    description: "Articles page route",
-  },
-  faq: {
-    id: "faq.path",
-    defaultMessage: "FAQ",
-    description: "FAQ page route",
-  },
-  contribute: {
-    id: "contribute.path",
-    defaultMessage: "Contribute",
-    description: "Contribute page route",
-  },
-  team: {
-    id: "team.path",
-    defaultMessage: "Team",
-    description: "Team page route",
-  },
-});
+}
 
 const routes: RouteInterface[] = [
   {
@@ -58,59 +22,32 @@ const routes: RouteInterface[] = [
   },
   {
     import: import("src/apps/main/pages/learn"),
-    path: messages.learn,
-    translate: true,
+    path: "/Learn",
   },
   {
     import: import("src/apps/main/pages/projects"),
-    path: messages.projects,
-    translate: true,
+    path: "/Projects",
   },
   {
     import: import("src/apps/main/pages/articles"),
-    path: messages.articles,
-    translate: true,
+    path: "/Articles",
   },
   {
     import: import("src/apps/main/pages/faq"),
-    path: messages.faq,
-    translate: true,
+    path: "/FAQ",
   },
   {
     import: import("src/apps/main/pages/contribute"),
-    path: messages.contribute,
-    translate: true,
+    path: "/Contribute",
   },
   {
     import: import("src/apps/main/pages/team"),
-    path: messages.team,
-    translate: true,
+    path: "/Team",
   },
   {
     import: import("src/apps/main/pages/not-found"),
   },
 ];
-const Routes: FC = () => {
-  const intl = useIntl();
-  return (
-    <Switch>
-      {routes.map((route, index) => (
-        <Route
-          exact={route.exact ? route.exact : undefined}
-          path={
-            route.translate
-              ? `/${intl.formatMessage(route.path)}`
-              : typeof route.path === "string"
-              ? "/"
-              : undefined
-          }
-          key={`route-${index}`}
-          component={lazy(() => route.import)}
-        />
-      ))}{" "}
-    </Switch>
-  );
-};
 
 export const App: FC = () => {
   if (getEnv() !== "development") {
@@ -128,17 +65,25 @@ export const App: FC = () => {
   return (
     <Theme>
       <ErrorBoundary>
-        <Localization>
-          <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
-            <Navbar />
-            <Container maxWidth="lg" style={{ paddingTop: "130px" }}>
-              <Suspense fallback={<Loading />}>
-                <Routes />
-              </Suspense>
-            </Container>
-            <Footer />
-          </div>
-        </Localization>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            minHeight: "100vh",
+          }}
+        >
+          <Navbar />
+          <Container maxWidth="lg" style={{ paddingTop: "130px" }}>
+            <Suspense fallback={<Loading />}>
+              <Switch>
+                {routes.map((route, index) => (
+                  <Route {...route} key={`route-${index}`} component={lazy(() => route.import)} />
+                ))}
+              </Switch>
+            </Suspense>
+          </Container>
+          <Footer />
+        </div>
       </ErrorBoundary>
     </Theme>
   );
