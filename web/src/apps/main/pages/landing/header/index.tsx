b1a5840235cf9c271bd1b91932bda2482776d29f
Zakaria Mansouri <zakman.dev@gmail.com>
2022-08-05 21:46:05 +0200

    localized and cleaned landing page

--- web/src/apps/main/pages/landing/header/index.tsx
+++ web/src/apps/main/pages/landing/header/index.tsx
@@ -5,8 +5,9 @@ import Hidden from "@material-ui/core/Hidden";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import Typography from "@material-ui/core/Typography";
 import { FC } from "react";
-import { FormattedMessage } from "react-intl";
 import image from "src/apps/main/assets/svg/dzcode.svg";
+import { Markdown } from "src/apps/main/components/markdown";
+import { T, t } from "src/apps/main/components/t";
 import { LinkV2 } from "src/components/link-v2";
 
 export const Header: FC = () => {
@@ -23,7 +24,6 @@ export const Header: FC = () => {
       },
     },
     title: {
-      // fontWeight: theme.typography.fontWeightBold,
       color: theme.palette.text.primary,
       textAlign: "center",
       fontSize: theme.typography.h5.fontSize,
@@ -33,13 +33,8 @@ export const Header: FC = () => {
     },
     callToAction: {
       width: "100%",
-
-      [theme.breakpoints.up("sm")]: {
-        marginRight: "50px",
-      },
     },
     button: {
-      // fontWeight: theme.typography.fontWeightBold,
       [theme.breakpoints.down("md")]: {
         width: "100%",
       },
@@ -68,12 +63,10 @@ export const Header: FC = () => {
         <Grid item xs={12} md={7}>
           <Box className={classes.left}>
             <Typography component="h1" className={classes.title}>
-              <FormattedMessage
-                id="landing.header.h"
-                defaultMessage="Algerian <span>Open Source</span> Community"
-                values={{
-                  span: (msg: string) => <span className={classes.greenHighlight}>{msg}</span>,
-                }}
+              <Markdown
+                t={t("landing-heading-title", {
+                  CLASS: classes.greenHighlight,
+                })}
               />
             </Typography>
 
@@ -83,26 +76,7 @@ export const Header: FC = () => {
               color="textPrimary"
               className={classes.description}
             >
-              <FormattedMessage
-                id="landing.header.body"
-                defaultMessage="Whether you are a maintainer or an open-source enthusiast DzCode helps you"
-              />{" "}
-              <LinkV2 href="/Projects">
-                <FormattedMessage id="landing.header.body.find" defaultMessage="find" />
-              </LinkV2>
-              ,{" "}
-              <LinkV2 href="/Contribute">
-                <FormattedMessage id="landing.header.body.contribute" defaultMessage="contribute" />{" "}
-              </LinkV2>
-              <FormattedMessage id="and" defaultMessage="and" />
-              <LinkV2 href="/Projects">
-                <FormattedMessage id="landing.header.body.list" defaultMessage="list" />{" "}
-              </LinkV2>
-              <FormattedMessage
-                id="landing.header.body.desc"
-                defaultMessage="open-source projects that solve Algerian
-              problems."
-              />
+              <Markdown t={t("landing-heading-subtitle")} />
             </Typography>
 
             <Box>
@@ -116,10 +90,7 @@ export const Header: FC = () => {
                   disableTouchRipple
                   className={classes.button}
                 >
-                  <FormattedMessage
-                    id="landing.button.contribute"
-                    defaultMessage="Make a Contribution"
-                  />
+                  <T landing-cta-button />
                 </Button>
               </LinkV2>
               <LinkV2 href="/FAQ">
@@ -133,7 +104,7 @@ export const Header: FC = () => {
                   size="large"
                   className={classes.button}
                 >
-                  <FormattedMessage id="landing.question" defaultMessage="Have a question ?" />
+                  <T landing-help-button />
                 </Button>
               </LinkV2>
             </Box>
@@ -142,9 +113,7 @@ export const Header: FC = () => {
 
         <Grid item xs={12} md={5}>
           <Hidden smDown>
-            <Box display="flex" width="100%" height="100%" alignItems="center">
-              <img src={image} alt="scrum board" />
-            </Box>
+            <img src={image} alt="scrum board" />
           </Hidden>
         </Grid>
       </Grid>
--- web/src/apps/main/pages/landing/index.tsx
+++ web/src/apps/main/pages/landing/index.tsx
@@ -1,65 +1,13 @@
 import { ErrorBoundary } from "@dzcode.io/ui/dist/error-boundary";
-import Button from "@material-ui/core/Button";
-import createStyles from "@material-ui/core/styles/createStyles";
-import makeStyles from "@material-ui/core/styles/makeStyles";
-import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
-import { FC, useEffect } from "react";
-import { FormattedMessage } from "react-intl";
-import { useDispatch } from "react-redux";
-import { Dispatch } from "src/apps/main/redux";
-import { fetchTopArticles, fetchTopProjects } from "src/apps/main/redux/actions/landing-page";
-import { LandingPageState } from "src/apps/main/redux/reducers/landing-page";
-import { LinkV2 } from "src/components/link-v2";
+import { FC } from "react";
 
 import { Header } from "./header";
 import { Mobile } from "./mobile";
-import { TopArticles } from "./top-articles";
-import { TopProjects } from "./top-projects";
-
-const useStyles = makeStyles((theme) =>
-  createStyles({
-    hr: {
-      maxWidth: "300px",
-      width: "90%",
-      marginBottom: theme.spacing(0),
-      marginTop: theme.spacing(0),
-    },
-    button: {
-      marginBottom: theme.spacing(4),
-      display: "inline-block",
-    },
-  }),
-);
 
 export const LandingPage: FC = () => {
-  const classes = useStyles();
-  const dispatch = useDispatch<Dispatch<LandingPageState>>();
-
-  useEffect(() => {
-    dispatch(fetchTopProjects());
-    dispatch(fetchTopArticles());
-  }, []);
-
   return (
     <ErrorBoundary>
       <Header />
-      <TopProjects />
-      <div style={{ textAlign: "center" }}>
-        <LinkV2 className={classes.button} href="/Projects">
-          <Button endIcon={<ArrowForwardIcon />} size="large">
-            <FormattedMessage id="landing.more.projects" defaultMessage="See More Projects" />
-          </Button>
-        </LinkV2>
-        <hr className={classes.hr} />
-      </div>
-      <TopArticles />
-      <div style={{ textAlign: "center" }}>
-        <LinkV2 className={classes.button} href="/Articles">
-          <Button endIcon={<ArrowForwardIcon />} size="large">
-            <FormattedMessage id="landing.more.articles" defaultMessage="Explore More Articles" />
-          </Button>
-        </LinkV2>
-      </div>
       <Mobile />
     </ErrorBoundary>
   );
