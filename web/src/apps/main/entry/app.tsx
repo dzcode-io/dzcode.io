befb72a648bee5f6b0679dd8e830d48887e3acb4
Zakaria Mansouri <zakman.dev@gmail.com>
2022-08-06 14:08:24 +0200

    made React Router support localization

--- web/src/apps/main/entry/app.tsx
+++ web/src/apps/main/entry/app.tsx
@@ -1,14 +1,20 @@
 import "./style.scss";
 
+import { allLanguages, LanguageEntity } from "@dzcode.io/models/dist/language";
 import { ErrorBoundary } from "@dzcode.io/ui/dist/error-boundary";
 import Container from "@material-ui/core/Container";
 import { ComponentType, FC, lazy, Suspense, useEffect } from "react";
-import { Route, RouteProps, Switch, useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Route, RouteProps, Switch, useLocation, useRouteMatch } from "react-router-dom";
 import { Footer } from "src/apps/main/components/footer";
 import { Navbar } from "src/apps/main/components/navbar";
 import { Theme } from "src/apps/main/components/theme";
 import { getEnv } from "src/common/utils";
+import { urlLanguageRegEx } from "src/common/utils/language";
 import { Loading } from "src/components/loading";
+
+import { Dispatch, StateInterface } from "../redux";
+import { SettingsState } from "../redux/reducers/settings";
 
 interface RouteInterface extends RouteProps {
   import: Promise<{ default: ComponentType }>;
@@ -50,17 +56,29 @@ const routes: RouteInterface[] = [
 ];
 
 export const App: FC = () => {
-  if (getEnv() !== "development") {
-    const location = useLocation();
+  const location = useLocation();
+  const match = useRouteMatch<{ lang?: LanguageEntity["code"] }>(urlLanguageRegEx);
+  const { language } = useSelector<StateInterface, SettingsState>((state) => state.settings);
+  const dispatch = useDispatch<Dispatch<SettingsState>>();
 
-    useEffect(() => {
+  useEffect(() => {
+    if (getEnv() !== "development") {
       if (window.ga) {
         window.ga("set", "page", location.pathname);
         window.ga("send", "pageview");
         window.fbq("track", "PageView");
       }
-    }, [location]);
-  }
+    }
+
+    const urlLanguage =
+      allLanguages.find(({ code }) => code === match?.params.lang) || allLanguages[0];
+    if (urlLanguage.code !== language.code) {
+      dispatch({
+        type: "UPDATE_SETTINGS",
+        payload: { language: urlLanguage },
+      });
+    }
+  }, [location]);
 
   return (
     <Theme>
@@ -76,8 +94,13 @@ export const App: FC = () => {
           <Container maxWidth="lg" style={{ paddingTop: "130px" }}>
             <Suspense fallback={<Loading />}>
               <Switch>
-                {routes.map((route, index) => (
-                  <Route {...route} key={`route-${index}`} component={lazy(() => route.import)} />
+                {routes.map(({ import: im, path, ...route }, index) => (
+                  <Route
+                    {...route}
+                    path={path ? `${urlLanguageRegEx}${path}` : undefined}
+                    key={`route-${index}`}
+                    component={lazy(() => im)}
+                  />
                 ))}
               </Switch>
             </Suspense>
