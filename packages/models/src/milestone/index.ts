1f1df53fa832c0ee7d9ffb65c4a8fb17b31166df
Zakaria Mansouri <zakman.dev@gmail.com>
2022-08-18 12:19:00 +0200

    added MilestoneEntity

---
+++ packages/models/src/milestone/index.ts
@@ -0,0 +1,37 @@
+import { IsDateString, IsNumber, IsOptional, IsString, IsUrl } from "class-validator";
+
+import { BaseEntity } from "../_base";
+
+export class MilestoneEntity extends BaseEntity {
+  @IsString()
+  id!: string;
+
+  @IsString()
+  title!: string;
+
+  @IsString()
+  description!: string;
+
+  @IsUrl()
+  url!: string;
+
+  @IsNumber()
+  openIssuesCount!: number;
+
+  @IsNumber()
+  closedIssuesCount!: number;
+
+  @IsString()
+  status!: "open" | "closed";
+
+  @IsDateString()
+  createdAt!: string;
+
+  @IsDateString()
+  @IsOptional()
+  dueAt?: string;
+
+  @IsDateString()
+  @IsOptional()
+  closedAt?: string;
+}
