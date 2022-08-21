86938c3b87c035f4b46ddc49b166248251c8673a
Zakaria Mansouri <zakman.dev@gmail.com>
2022-08-21 13:10:14 +0200

    made Milestones component mobile friendly

--- packages/ui/src/milestones/index.tsx
+++ packages/ui/src/milestones/index.tsx
@@ -1,3 +1,4 @@
+import CircleIcon from "@mui/icons-material/Circle";
 import Timeline from "@mui/lab/Timeline";
 import TimelineConnector from "@mui/lab/TimelineConnector";
 import TimelineContent from "@mui/lab/TimelineContent";
@@ -5,7 +6,14 @@ import TimelineDot from "@mui/lab/TimelineDot";
 import TimelineItem from "@mui/lab/TimelineItem";
 import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
 import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import Step from "@mui/material/Step";
+import StepContent from "@mui/material/StepContent";
+import StepIcon from "@mui/material/StepIcon";
+import StepLabel from "@mui/material/StepLabel";
+import Stepper from "@mui/material/Stepper";
+import { useTheme } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import type { FC } from "react";
 
 import { CircularProgress } from "../circular-progress";
@@ -26,46 +34,84 @@ interface MilestonesProps {
 
 // @TODO-ZM: update colors
 const milestoneColors: Record<MilestoneItem["state"], string> = {
-  open: "grey",
+  open: "silver",
   closed: "green",
   "in-progress": "primary.main",
 };
 
 // @TODO-ZM: move this to @dzcode.io/ui and move deps with it (@mui/lab & @mui/material)
 export const Milestones: FC<MilestonesProps> = ({ milestones, onClick = () => null }) => {
+  const large = useMediaQuery("(min-width:600px)");
+  const theme = useTheme();
+
+  console.log(theme.palette);
+
   return milestones ? (
-    <Timeline position="alternate" sx={{ marginTop: 3, marginBottom: 3 }}>
-      {milestones.map(({ id, title, description, date, state, progress }, milestoneIndex) => (
-        <TimelineItem key={id}>
-          {date ? (
-            <TimelineOppositeContent sx={{ m: "auto 0" }} align="right" variant="body2">
-              {/* @TODO-ZM: localize this  */}
-              {new Date(date).toLocaleDateString()}
-            </TimelineOppositeContent>
-          ) : null}
-          <TimelineSeparator>
-            <TimelineConnector sx={{ bgcolor: milestoneColors[state] }} />
-            {state === "in-progress" ? (
-              <CircularProgress style={{ margin: 8 }} value={progress || 0} />
-            ) : (
-              <TimelineDot sx={{ bgcolor: milestoneColors[state] }} />
-            )}
-            <TimelineConnector sx={{ bgcolor: milestoneColors[state] }} />
-          </TimelineSeparator>
-          <TimelineContent sx={{ py: "12px", px: 2 }}>
-            <Typography
-              variant="h6"
-              component="span"
-              onClick={() => onClick(milestoneIndex, milestones[milestoneIndex])}
-              sx={{ cursor: "pointer" }}
+    large ? (
+      <Timeline position="alternate" sx={{ marginTop: 3, marginBottom: 3 }}>
+        {milestones.map(({ id, title, description, date, state, progress }, milestoneIndex) => (
+          <TimelineItem key={id}>
+            {date ? (
+              <TimelineOppositeContent sx={{ m: "auto 0" }} align="right" variant="body2">
+                {/* @TODO-ZM: localize this  */}
+                {new Date(date).toLocaleDateString()}
+              </TimelineOppositeContent>
+            ) : null}
+            <TimelineSeparator>
+              <TimelineConnector sx={{ bgcolor: milestoneColors[state] }} />
+              {state === "in-progress" ? (
+                <CircularProgress style={{ margin: 8 }} value={progress || 0} />
+              ) : (
+                <TimelineDot sx={{ bgcolor: milestoneColors[state] }} />
+              )}
+              <TimelineConnector sx={{ bgcolor: milestoneColors[state] }} />
+            </TimelineSeparator>
+            <TimelineContent sx={{ py: "12px", px: 2 }}>
+              <Typography
+                variant="h6"
+                component="span"
+                onClick={() => onClick(milestoneIndex, milestones[milestoneIndex])}
+                sx={{ cursor: "pointer" }}
+              >
+                {title}
+              </Typography>
+              {description && <Typography>{description}</Typography>}
+            </TimelineContent>
+          </TimelineItem>
+        ))}
+      </Timeline>
+    ) : (
+      <Stepper orientation="vertical" connector={null} sx={{ margin: 3 }}>
+        {milestones.map(({ id, title, description, date, state, progress }, milestoneIndex) => (
+          <Step key={id} active={true} color="primary">
+            <StepLabel
+              StepIconComponent={() =>
+                state === "in-progress" ? (
+                  <CircularProgress style={{ marginLeft: -8 }} value={progress || 0} />
+                ) : (
+                  <StepIcon icon={<CircleIcon sx={{ color: milestoneColors[state] }} />} />
+                )
+              }
             >
-              {title}
-            </Typography>
-            {description && <Typography>{description}</Typography>}
-          </TimelineContent>
-        </TimelineItem>
-      ))}
-    </Timeline>
+              <Typography
+                variant="h6"
+                component="span"
+                onClick={() => onClick(milestoneIndex, milestones[milestoneIndex])}
+                sx={{ cursor: "pointer", color: theme.palette.text.primary }}
+              >
+                {title}
+              </Typography>
+            </StepLabel>
+            <StepContent sx={{ borderLeftColor: milestoneColors[state], paddingBottom: 2 }}>
+              {date && (
+                <Typography variant="body2">{new Date(date).toLocaleDateString()}</Typography>
+              )}
+              <Typography>{description}</Typography>
+            </StepContent>
+          </Step>
+        ))}
+      </Stepper>
+    )
   ) : (
     // @TODO-ZM: render a skeleton
     <div>Loading ...</div>
