a684d8c6b7fc7e954270aef97c7d72683c43a701
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-08-28 11:09:39 +0100

    define store structure

---
+++ mobile/src/store/learn-screen/slice.ts
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const learnScreenSlice = createSlice({
+  name: "learnScreen",
+  initialState: {},
+  reducers: {},
+});
+
+export default learnScreenSlice;
---
+++ mobile/src/store/projects-screen/slice.ts
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const projectsScreenSlice = createSlice({
+  name: "projectsScreen",
+  initialState: {},
+  reducers: {},
+});
+
+export default projectsScreenSlice;
