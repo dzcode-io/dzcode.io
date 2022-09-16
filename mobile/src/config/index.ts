07fbcbbf33bbe718abbcbdbe140c4850363a83b4
Omar <bel_omar18@yahoo.com>
2022-09-16 22:52:28 +0100

    update `mobile/src/config/index.ts`
    Co-authored-by: Zakaria Mansouri <zakman.dev@gmail.com>

--- mobile/src/config/index.ts
+++ mobile/src/config/index.ts
@@ -1,6 +1,4 @@
 import { fsConfig } from "@dzcode.io/utils/dist/config";
 import { getEnv } from "utils/env";
 
-export const fullstackConfig = fsConfig(getEnv(), {
-  hostname: process.env.LOCAL_API_HOST,
-});
+export const fullstackConfig = fsConfig(getEnv(), { hostname: process.env.LOCAL_API_HOST });
