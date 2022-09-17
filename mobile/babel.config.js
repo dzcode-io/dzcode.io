20785fed7f7805d6fdb970087d537cf1c850d4b4
omdxp <omarbelghaouti@gmail.com>
2022-09-17 10:54:12 +0100

    change `root` in `module-resolver`

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -24,7 +24,7 @@ module.exports = function (api) {
       [
         "module-resolver",
         {
-          root: ["./"],
+          root: ["../"],
           alias: {
             src: "./src",
           },
