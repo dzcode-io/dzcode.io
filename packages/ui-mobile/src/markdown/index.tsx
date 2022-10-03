fdbd873328f8bb81037c0b1dd94ab8e929d24ab8
omdxp <omarbelghaouti@gmail.com>
2022-10-03 10:36:59 +0100

    get rid of `fontFamily` error

--- packages/ui-mobile/src/markdown/index.tsx
+++ packages/ui-mobile/src/markdown/index.tsx
@@ -1,5 +1,8 @@
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from "@expo-google-fonts/roboto";
 import React, { VFC } from "react";
+import { View } from "react-native";
 import { default as MarkdownDisplay, MarkdownIt } from "react-native-markdown-display";
+import { DZCodeLoading } from "src/loading";
 import { LARGE_MARGIN_SIZE } from "src/utils/constants";
 
 interface MarkdownProps {
@@ -9,43 +12,66 @@ interface MarkdownProps {
 }
 
 export const Markdown: VFC<MarkdownProps> = ({ content, theme = "light", onLinkPress }) => {
-  return (
-    <MarkdownDisplay
-      markdownit={MarkdownIt({ typographer: true, linkify: true })}
-      style={{
-        text: {
-          color: theme === "dark" ? "white" : "black",
-        },
-        /* eslint-disable camelcase */
-        bullet_list: {
-          color: theme === "dark" ? "white" : "black",
-        },
-        ordered_list: {
-          color: theme === "dark" ? "white" : "black",
-        },
-        fence: {
-          color: theme === "dark" ? "white" : "black",
-          backgroundColor: "transparent",
-        },
-        blockquote: {
-          color: theme === "dark" ? "white" : "black",
-          backgroundColor: "transparent",
-        },
-        code_inline: {
-          color: theme === "dark" ? "white" : "black",
-          backgroundColor: "transparent",
-        },
-        body: {
-          marginHorizontal: LARGE_MARGIN_SIZE,
-        },
-        /* eslint-enable camelcase */
-      }}
-      onLinkPress={(url) => {
-        onLinkPress?.(url);
-        return true;
-      }}
-    >
-      {content}
-    </MarkdownDisplay>
-  );
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <DZCodeLoading />
+      </View>
+    );
+  } else {
+    return (
+      <MarkdownDisplay
+        markdownit={MarkdownIt({ typographer: true, linkify: true })}
+        style={{
+          text: {
+            color: theme === "dark" ? "white" : "black",
+          },
+          /* eslint-disable camelcase */
+          bullet_list: {
+            color: theme === "dark" ? "white" : "black",
+          },
+          ordered_list: {
+            color: theme === "dark" ? "white" : "black",
+          },
+          fence: {
+            color: theme === "dark" ? "white" : "black",
+            backgroundColor: "transparent",
+            fontFamily: "Roboto_400Regular",
+          },
+          blockquote: {
+            color: theme === "dark" ? "white" : "black",
+            backgroundColor: "transparent",
+            fontFamily: "Roboto_400Regular",
+          },
+          code_inline: {
+            color: theme === "dark" ? "white" : "black",
+            backgroundColor: "transparent",
+            fontFamily: "Roboto_400Regular",
+          },
+          body: {
+            marginHorizontal: LARGE_MARGIN_SIZE,
+            fontFamily: "Roboto_400Regular",
+          },
+          /* eslint-enable camelcase */
+        }}
+        onLinkPress={(url) => {
+          onLinkPress?.(url);
+          return true;
+        }}
+      >
+        {content}
+      </MarkdownDisplay>
+    );
+  }
 };
