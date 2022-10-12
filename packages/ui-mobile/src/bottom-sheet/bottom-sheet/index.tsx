62921172e5ca628355765b7a3f4bc406e3156b1e
omdxp <omarbelghaouti@gmail.com>
2022-10-12 15:22:07 +0100

    docs: add `BottomSheet` component doc

--- packages/ui-mobile/src/bottom-sheet/bottom-sheet/index.tsx
+++ packages/ui-mobile/src/bottom-sheet/bottom-sheet/index.tsx
@@ -4,6 +4,14 @@ import { useTheme } from "src/_hooks/use-theme";
 
 import { bottomSheetStyles } from "./styles";
 
+/**
+ * BottomSheet component used to display the bottom sheet in the app
+ * @prop {React.ReactNode} children - the children of the bottom sheet
+ * @example
+ * <BottomSheet>
+ *  <Text>Bottom Sheet Content</Text>
+ * </BottomSheet>
+ */
 export const BottomSheet: FC = ({ children }) => {
   const { colors } = useTheme();
   return (
