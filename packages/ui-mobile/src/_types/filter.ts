e907b2ccb6351692ce3dd57828424fc04f357ef4
omdxp <omarbelghaouti@gmail.com>
2022-10-12 14:38:26 +0100

    docs: add `Filter` interface doc

--- packages/ui-mobile/src/_types/filter.ts
+++ packages/ui-mobile/src/_types/filter.ts
@@ -1,5 +1,20 @@
 import { Option } from "./option";
 
+/**
+ * Type used in `@dzcode.io/api` package to filter contributions
+ * @example
+ * let filter: Filter = {
+ *  name: "filter",
+ *  label: "Filter",
+ *  options: [
+ *    {
+ *      label: "All",
+ *      name: "all",
+ *      checked: true,
+ *    },
+ *  ],
+ * };
+ */
 export interface Filter {
   label: string;
   name: string;
