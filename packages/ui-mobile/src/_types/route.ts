6a84fe4e805116231b6042e2198497d51affebac
omdxp <omarbelghaouti@gmail.com>
2022-10-12 14:56:19 +0100

    docs: add `Route` interface doc

--- packages/ui-mobile/src/_types/route.ts
+++ packages/ui-mobile/src/_types/route.ts
@@ -1,3 +1,29 @@
+/**
+ * Type used to define navigation routes
+ * @example
+ * import type { Route } from "@dzcode.io/ui-mobile/dist/_types/route";
+ * import { StackNav } from "@dzcode.io/ui-mobile/dist/navigation/stack-nav";
+ * import { FC } from "react";
+ * import { DocumentDetailsScreen } from "./document-details";
+ * import { DocumentsListScreen } from "./documents-list";
+ *
+ * const routes: Route[] = [
+ *  {
+ *    name: "documents-list",
+ *    component: DocumentsListScreen,
+ *  },
+ *  {
+ *    name: "document-details",
+ *    component: DocumentDetailsScreen,
+ *   },
+ * ];
+ *
+ * export const Navigation: FC = () => {
+ *  return (
+ *    <StackNav routes={routes} headerMode={"none"} initialRouteName={"documents-list"} />
+ *  );
+ * };
+ */
 export interface Route {
   name: string;
   title?: string;
