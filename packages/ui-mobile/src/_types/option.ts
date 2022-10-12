466fe8195b87961f43bfee48382d3b5092e1ec3f
omdxp <omarbelghaouti@gmail.com>
2022-10-12 14:46:49 +0100

    docs: add `Option` interface doc

--- packages/ui-mobile/src/_types/option.ts
+++ packages/ui-mobile/src/_types/option.ts
@@ -1,3 +1,12 @@
+/**
+ * Type used in `@dzcode.io/api` package to help with filtering contributions
+ * @example
+ * let option: Option = {
+ *  label: "All",
+ *  name: "all",
+ *  checked: true,
+ * };
+ */
 export interface Option {
   label: string;
   name: string;
