cd66738eae47f1721e08164407525f332566a63a
omdxp <omarbelghaouti@gmail.com>
2022-10-12 15:20:31 +0100

    docs: add `AppBar` component doc

--- packages/ui-mobile/src/app-bar/index.tsx
+++ packages/ui-mobile/src/app-bar/index.tsx
@@ -1,12 +1,28 @@
 import React, { FC } from "react";
 import { Appbar, useTheme } from "react-native-paper";
 
-interface AppbarProps {
+interface AppBarProps {
   title: string;
   openDrawer: VoidFunction;
 }
 
-export const AppBar: FC<AppbarProps> = ({ title, openDrawer }) => {
+/**
+ * AppBar component used to display the app bar in the app
+ * @prop {string} title - the title of the app bar
+ * @prop {VoidFunction} openDrawer - a function that opens the drawer
+ * @example
+ * <DrawerNav
+ *    routes={routes}
+ *    initialRouteName="contribute"
+ *    header={(props) => (
+ *      <AppBar
+ *        title={routes.find(({ name }) => name === props.scene.route.name)?.title || ""}
+ *        openDrawer={() => props.scene.descriptor.navigation.dispatch(DrawerActions.openDrawer())}
+ *      />
+ *    )}
+ * />
+ */
+export const AppBar: FC<AppBarProps> = ({ title, openDrawer }) => {
   const theme = useTheme();
 
   return (
