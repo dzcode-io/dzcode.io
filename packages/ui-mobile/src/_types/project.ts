44d6511707cc6749111064d573c184db12035711
omdxp <omarbelghaouti@gmail.com>
2022-10-12 14:51:09 +0100

    docs: add `Project` interface doc

--- packages/ui-mobile/src/_types/project.ts
+++ packages/ui-mobile/src/_types/project.ts
@@ -1,3 +1,12 @@
+/**
+ * Type used in `@dzcode.io/api` package to represent a project
+ * @example
+ * let project: Project = {
+ *  slug: "dzcode-io",
+ *  description: "dzcode.io is an open source project that aims to provide a platform for Algerian developers to share their knowledge and experience with the community.",
+ *  githubURI: "https://github.com/dzcode-io/dzcode.io"
+ * };
+ */
 export interface Project {
   slug: string;
   image?: string;
