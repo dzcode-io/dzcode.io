3a8fa82c26d042ed8739cce102885afed0be84f0
Zakaria Mansouri <zakman.dev@gmail.com>
2022-11-19 18:29:46 +0100

    removed duplicated loaded function in utils

--- packages/utils/src/loadable/index.ts
+++ packages/utils/src/loadable/index.ts
@@ -1,13 +1,3 @@
 export type LOADABLE<T> = null | "ERROR" | T;
 
 export const isLoaded = <T>(loadable: LOADABLE<T>) => (loadable !== "ERROR" && loadable) || null;
-
-export const loaded = <T>(loadable: LOADABLE<T>) => {
-  if (loadable === "ERROR") {
-    return null;
-  }
-  if (!loadable) {
-    return null;
-  }
-  return loadable;
-};
