37ae52857cc177d2961af5abfee1f80268a6b9fd
omdxp <omarbelghaouti@gmail.com>
2023-01-12 15:21:35 +0100

    chore: update the fetch url

--- mobile/src/redux/actions/projects-screen/index.ts
+++ mobile/src/redux/actions/projects-screen/index.ts
@@ -4,7 +4,7 @@ import { shuffleArray } from "src/utils/shuffle";
 
 export const fetchProjects = createAsyncThunk("projectsScreen/fetchProjects", async () => {
   try {
-    const projects = await fetchV2("data:projects/list.c.json", {});
+    const projects = await fetchV2("data:projects-v2/list.c.json", {});
     return shuffleArray(projects);
   } catch (error: any) {
     return error.message;
