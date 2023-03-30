20505496c67034374e8c23db8a4cf357f916065a
omdxp <omarbelghaouti@gmail.com>
2023-03-30 10:53:01 +0200

    address requested changes

--- web/cypress.config.ts
+++ web/cypress.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   e2e: {
     baseUrl: fsConfig("development").web.url,
     supportFile: "src/_e2e-test/support/e2e.ts",
-    specPattern: "src/_e2e-test/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    specPattern: "src/_e2e-test/tests/**/*.spec.{js,jsx,ts,tsx}",
     downloadsFolder: "src/_e2e-test/downloads",
     videosFolder: "src/_e2e-test/videos",
     screenshotsFolder: "src/_e2e-test/screenshots",
