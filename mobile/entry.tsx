f144ded2837fd111e1c7533d502006d4df8c8b61
Zakaria Mansouri <zakman.dev@gmail.com>
2023-04-01 14:04:46 +0200

    react 18

--- mobile/entry.tsx
+++ mobile/entry.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export const App = () => {
   return (
     <View style={styles.container}>
-      <Text>zak zako </Text>
+      <Text>okok okokokhhh </Text>
       <StatusBar style="auto" />
     </View>
   );
