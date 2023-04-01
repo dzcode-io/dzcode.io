1072607e6c72c7a2661357a07239e2a3c405e0ea
Zakaria Mansouri <zakman.dev@gmail.com>
2023-04-01 13:34:58 +0200

    added expo 18 app to workspace

--- mobile/entry.tsx
+++ mobile/entry.tsx
@@ -1,14 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+export const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to</Text>
+      <Text>zak zako </Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
