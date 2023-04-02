3773bf236f428cb500614704b3fa67d017707128
Zakaria Mansouri <zakman.dev@gmail.com>
2023-04-02 21:52:50 +0200

    moved entry to src/_entry/index.js

--- mobile/src/_entry/app.tsx
+++ mobile/src/_entry/app.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export const App = () => {
   return (
     <View style={styles.container}>
-      <Text>okok okokokhhh </Text>
+      <Text>moved entry</Text>
       <StatusBar style="auto" />
     </View>
   );
