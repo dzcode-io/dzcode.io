0ff4da59f78cdd6d21bba29c20e1476c214c462b
Zakaria Mansouri <zakman.dev@gmail.com>
2023-04-03 02:06:52 +0200

    removed old code

--- mobile-old/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: "jest-expo",
-  transformIgnorePatterns: [
-    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|@sentry|native-base|react-native-svg)",
-  ],
-};
--- mobile-old/src/redux/reducers/projects-screen/adapters/projects.ts
+++
@@ -1,6 +0,0 @@
-import { Project } from "@dzcode.io/api/dist/app/types/legacy";
-import { createEntityAdapter } from "@reduxjs/toolkit";
-
-export const projectsAdapter = createEntityAdapter<Project>({
-  selectId: (project) => project.slug,
-});
