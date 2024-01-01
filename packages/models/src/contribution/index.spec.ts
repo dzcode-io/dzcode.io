559cc0edf3f2a67d2557df6072734b323c4f70b9
Zakaria Mansouri <zakman.dev@gmail.com>
2024-01-01 16:48:30 +0100

    Add createdBy field to ContributionEntity

--- packages/models/src/contribution/index.spec.ts
+++ packages/models/src/contribution/index.spec.ts
@@ -18,6 +18,13 @@ runDTOTestCases(
     createdAt: "2020-02-02T20:22:02.000Z",
     updatedAt: "2020-02-02T20:22:02.000Z",
     url: "https://github.com/dzcode-io/leblad/issues/71",
+    createdBy: {
+      id: "github/20110076",
+      username: "ZibanPirate",
+      name: "Zakaria Mansouri",
+      profileUrl: "/Account/github/20110076",
+      avatarUrl: "https://avatars.githubusercontent.com/u/20110076?v=4",
+    },
   },
   {},
 );
