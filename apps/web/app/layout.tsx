57704bf0e5730c73b8e953d33d046e7d4074c80d
Zakaria Mansouri <zakman.dev@gmail.com>
2024-03-28 17:20:22 +0100

    added daisyui

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,3 +1,4 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
@@ -8,11 +9,7 @@ export const metadata: Metadata = {
   description: "Algerian Open-source Community",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}): JSX.Element {
+export default function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
