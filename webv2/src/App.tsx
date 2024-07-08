791c238e0e5ca2221bb686f4aa45e07fe7ad71d6
Zakaria Mansouri <zakman.dev@gmail.com>
2024-07-08 15:49:57 +0200

    chore: Add Tailwind CSS configuration files and update dependencies

--- webv2/src/App.tsx
+++ webv2/src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="content">
+    <div>
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
     </div>
