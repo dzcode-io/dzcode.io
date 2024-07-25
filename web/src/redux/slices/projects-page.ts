5dcf5d5ae580b21524b4db4bd2836757505644c2
Zakaria Mansouri <zakman.dev@gmail.com>
2024-07-25 17:39:52 +0200

    got project types from API

--- web/src/redux/slices/projects-page.ts
+++ web/src/redux/slices/projects-page.ts
@@ -1,27 +1,10 @@
+import { GetProjectsResponseDto } from "@dzcode.io/api/dist/project/types";
 import { createSlice } from "@reduxjs/toolkit";
 import { setReducerFactory } from "src/redux/utils";
 import { LOADABLE } from "src/utils/loadable";
 
 export interface ProjectsPageState {
-  // @TODO: get from ./api
-  projectsList: LOADABLE<{
-    projects: Array<{
-      name: string;
-      repositories: Array<{
-        provider: string;
-        owner: string;
-        repository: string;
-        stats: {
-          contributionCount: number;
-          languages: Array<string>;
-        };
-        contributors: Array<{
-          name: string;
-          avatarUrl: string;
-        }>;
-      }>;
-    }>;
-  }>;
+  projectsList: LOADABLE<GetProjectsResponseDto["projects"]>;
 }
 
 const initialState: ProjectsPageState = {
