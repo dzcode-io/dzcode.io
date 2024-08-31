235b67416d18a82fd125009727e5d667cd0e05ea
Zakaria Mansouri <zakman.dev@gmail.com>
2024-08-31 23:18:07 +0200

    chore: removed old web

--- _old_web/src/components/l/index.tsx
+++
@@ -1,5 +0,0 @@
-import { loadableFactory } from "@dzcode.io/ui/dist/loadable-factory";
-
-export const L = loadableFactory<{ page: string }>((props) => import(`src/pages/${props.page}`), {
-  cacheKey: (props) => props.page,
-});
--- _old_web/src/utils/language.ts
+++
@@ -1,3 +0,0 @@
-import { allLanguages } from "@dzcode.io/models/dist/language";
-
-export const urlLanguageRegEx = `/:lang(${allLanguages.map(({ code }) => code).join("|")})?`;
