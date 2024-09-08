6552e14fb1ccfa0d7297fd6074f3a261da64f84c
Zakaria Mansouri <zakman.dev@gmail.com>
2024-09-08 20:06:24 +0200

    remove more unused code

--- api/src/data/types.ts
+++ api/src/data/types.ts
@@ -1,6 +1,3 @@
-import { Model } from "@dzcode.io/models/dist/_base";
-import { ArticleInfoEntity } from "@dzcode.io/models/dist/article";
-import { DocumentationInfoEntity } from "@dzcode.io/models/dist/documentation";
 import { RepositoryEntity } from "@dzcode.io/models/dist/repository";
 
 export type DataProjectEntity = {
@@ -11,18 +8,4 @@ export type DataProjectEntity = {
     owner: string;
     name: string;
   }>;
-};
-
-export type DataArticleEntity = Model<ArticleInfoEntity> & {
-  authors: string[];
-  description: string;
-  content: string;
-  image: string;
-};
-
-export type DataDocumentationEntity = Model<DocumentationInfoEntity> & {
-  authors: string[];
-  description: string;
-  content: string;
-  image: string;
 };
