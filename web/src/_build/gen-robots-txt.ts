f4d284ad200fe2d0eee8516167673048e59666c0
Zakaria Mansouri <zakman.dev@gmail.com>
2024-09-18 23:50:39 +0200

    add robot.txt to `./web`

---
+++ web/src/_build/gen-robots-txt.ts
@@ -0,0 +1,27 @@
+// for dev, run:
+// npm run clean && npm run bundle && npm run generate:robots-txt
+// npm run rsbuild preview
+
+import { join } from "path";
+import { writeFileSync } from "fs";
+import { Environment, environments } from "@dzcode.io/utils/dist/config/environment";
+
+let stage = process.env.STAGE as Environment;
+if (!environments.includes(stage)) {
+  console.log(`⚠️  No STAGE provided, falling back to "development"`);
+  stage = "development";
+}
+
+const distFolder = "./bundle";
+
+const robotsTxtPath = join(distFolder, "robots.txt");
+
+console.log(`generating $robot.txt ...`);
+
+const robotsTxt = `User-agent: *
+${stage === "production" ? "Allow: /" : "Disallow: /"}
+`;
+
+writeFileSync(robotsTxtPath, robotsTxt);
+
+console.log("done");
