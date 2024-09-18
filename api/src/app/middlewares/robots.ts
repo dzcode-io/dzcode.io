9ec7775cc110e36015539df788500562afb213c7
Zakaria Mansouri <zakman.dev@gmail.com>
2024-09-18 23:51:14 +0200

    fix robot.txt to `./api`

--- api/src/app/middlewares/robots.ts
+++ api/src/app/middlewares/robots.ts
@@ -1,10 +1,22 @@
-import { RequestHandler } from "express";
-import { ExpressMiddlewareInterface, Middleware } from "routing-controllers";
+import { ConfigService } from "src/config/service";
 import { Service } from "typedi";
-const robots = require("express-robots-txt"); // eslint-disable-line @typescript-eslint/no-require-imports
+import { Response } from "express";
+import { Controller, Res, Get } from "routing-controllers";
 
 @Service()
-@Middleware({ type: "before", priority: 0 })
-export class RobotsMiddleware implements ExpressMiddlewareInterface {
-  use: RequestHandler = robots({ UserAgent: "*", Disallow: "/docs" });
+@Controller("/robots.txt")
+export class RobotsController {
+  private readonly stage = this.configService.env().NODE_ENV;
+
+  constructor(private readonly configService: ConfigService) {}
+
+  @Get("/")
+  public async robotsTxt(@Res() response: Response): Promise<Response> {
+    response.set("Content-Type", "text/plain");
+    if (this.stage === "production") {
+      return response.send("User-agent: *\nAllow: /");
+    } else {
+      return response.send("User-agent: *\nDisallow: /");
+    }
+  }
 }
