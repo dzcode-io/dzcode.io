4d7e91dcaefd169b9e0db02cdeecd16df361cb63
Zakaria Mansouri <zakman.dev@gmail.com>
2024-09-22 00:39:48 +0200

    don't rate limit in development

--- api/src/app/middlewares/security.ts
+++ api/src/app/middlewares/security.ts
@@ -21,12 +21,13 @@ export class SecurityMiddleware implements ExpressMiddlewareInterface {
 
     this.router.use(helmet());
 
-    this.router.use(
-      rateLimit({
-        windowMs: 15 * 60 * 1000, // 15 minutes
-        max: 100, // limit each IP to 100 requests per windowMs
-      }),
-    );
+    if (this.env !== "development")
+      this.router.use(
+        rateLimit({
+          windowMs: 15 * 60 * 1000, // 15 minutes
+          max: 100, // limit each IP to 100 requests per windowMs
+        }),
+      );
   }
 
   private router = Router();
