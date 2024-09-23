48587183bf5bd45c5aab608a9e119a39a3eec461
Zakaria Mansouri <zakman.dev@gmail.com>
2024-09-23 13:32:08 +0200

    first stab at dynamic html

--- web/cloudflare/public/functions/projects/[slug].ts
+++ web/cloudflare/public/functions/projects/[slug].ts
@@ -1,3 +1,15 @@
-export function onRequest() {
-  return new Response("Hello, world!");
-}
+declare const data: string;
+// @ts-expect-error cloudflare converts this to a string using esbuild
+import data from "../../index.html";
+
+export const onRequest: PagesFunction = (context) => {
+  const pathName = new URL(context.request.url).pathname;
+  const slug = pathName.split("/").pop();
+  const newData = data.replace(/<title>.*<\/title>/, `<title>Project ${slug} at DzCode</title>`);
+
+  return new Response(newData, {
+    headers: {
+      "content-type": "text/html; charset=utf-8",
+    },
+  });
+};
