cbc25f557284354c27341525334f14e35913453b
Zakaria Mansouri <zakman.dev@gmail.com>
2024-09-23 10:04:24 +0200

    relaxed rate limit

--- api/src/app/middlewares/security.ts
+++ api/src/app/middlewares/security.ts
@@ -24,8 +24,8 @@ export class SecurityMiddleware implements ExpressMiddlewareInterface {
     if (this.env !== "development")
       this.router.use(
         rateLimit({
-          windowMs: 15 * 60 * 1000, // 15 minutes
-          max: 100, // limit each IP to 100 requests per windowMs
+          windowMs: 1 * 60 * 1000, // 1 minute
+          max: 1000, // limit each IP to 1000 requests per windowMs
         }),
       );
   }
