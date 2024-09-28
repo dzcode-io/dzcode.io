f43cab239857f497bdf2b96cd46fad2c7ba2cd30
Zakaria Mansouri <zakman.dev@gmail.com>
2024-09-28 23:08:55 +0200

    genrate and use wrangler.toml

---
+++ web/cloudflare/scripts/generate-config.ts
@@ -0,0 +1,21 @@
+import { writeFileSync } from "fs";
+
+// Get the environment passed as argument
+const STAGE = process.argv[2];
+if (!STAGE) throw new Error("Please provide a STAGE");
+
+console.log(`Creating wrangler.toml ...`);
+
+writeFileSync(
+  "wrangler.toml",
+  `
+name = ${STAGE === "production" ? '"dzcode-dot-io"' : '"stage-dot-dzcode-dot-io"'}
+compatibility_date = "2024-09-28"
+pages_build_output_dir = "./public"
+
+[vars]
+STAGE = "${STAGE}"
+`,
+);
+
+console.log(`Done creating .bundle-info.json`);
