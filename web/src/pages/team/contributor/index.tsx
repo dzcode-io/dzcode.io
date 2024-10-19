ccfcda10c110db6e17a5a1a78e888579eeeb7480
Zakaria Mansouri <zakman.dev@gmail.com>
2024-10-19 14:03:44 +0200

    feat: added black contributor page

---
+++ web/src/pages/team/contributor/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+// ts-prune-ignore-next
+export default function Page(): JSX.Element {
+  return <main className="flex flex-col self-center w-full max-w-7xl">contributor page</main>;
+}
