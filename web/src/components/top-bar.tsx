d797ff59583f6b8148e5c4e094fcb4598696fb3d
Omar <bel_omar18@yahoo.com>
2025-04-09 11:41:48 +0200

    Update web/src/components/top-bar.tsx

--- web/src/components/top-bar.tsx
+++ web/src/components/top-bar.tsx
@@ -133,9 +133,7 @@ export function TopBar({ version, links }: TopBarProps): JSX.Element {
               <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
             </svg>
             <input
-              onClick={() => {
-                toggleTheme();
-              }}
+              onClick={toggleTheme}
               id="theme-toggle"
               type="checkbox"
               value="dzcodeLight"
