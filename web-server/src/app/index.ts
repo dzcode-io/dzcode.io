bee41959bac55e8b1907e4e557020cf38fbc7964
Zakaria Mansouri <zakman.dev@gmail.com>
2025-04-26 22:17:25 +0200

    added dynamic routes and fallback to static content

--- web-server/src/app/index.ts
+++ web-server/src/app/index.ts
@@ -8,6 +8,48 @@ const port = process.env.PORT || 6060;
 const staticPath = path.join(__dirname, "../../../web/bundle");
 const indexPath = path.join(staticPath, "index.html");
 
+app.get("/w/contributions-sitemap.xml", (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.get("/w/contributors-sitemap.xml", (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.get("/w/projects-sitemap.xml", (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.get(/^\/(?:([a-z]{2})\/)?projects\/(.*)$/, (req, res) => {
+  const lang = req.params[0] || "en";
+  const projectId = req.params[1];
+
+  res.json({
+    lang,
+    projectId,
+  });
+});
+
+app.get(/^\/(?:([a-z]{2})\/)?contribute\/(.*)$/, (req, res) => {
+  const lang = req.params[0] || "en";
+  const contributionId = req.params[1];
+
+  res.json({
+    lang,
+    contributionId,
+  });
+});
+
+app.get(/^\/(?:([a-z]{2})\/)?team\/(.*)$/, (req, res) => {
+  const lang = req.params[0] || "en";
+  const contributorId = req.params[1];
+
+  res.json({
+    lang,
+    teamId: contributorId,
+  });
+});
+
 app.use(express.static(staticPath));
 
 app.use((req, res) => {
